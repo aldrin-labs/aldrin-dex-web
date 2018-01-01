c09fa65ec0b0bfb565a89d9e9877eee19c932ba1
Bogdan Volyk <bogdan.volyk@gmail.com>
2018-01-01 22:46:17 +0200

    :wrench: Webpack flow and yarn fix

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -14,7 +14,7 @@ const config = {
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './app.js',
+    './index.js',
   ],
 
   output: {
