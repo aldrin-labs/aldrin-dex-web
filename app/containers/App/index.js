86c74c4d310d38c5ee3a607f7c6968b24103f210
Bogdan Volyk <bogdan.volyk@gmail.com>
2018-01-01 23:04:58 +0200

    :sparkles: Material works now!

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import styled, { ThemeProvider } from 'styled-components'
 
 import { HomePage } from '../../components'
+import TestComp from '../TestComp'
 import theme from '../../components/themes/default'
 
 const AppContainer = styled.div`
@@ -28,16 +29,14 @@ const AnotherSamplePage = () => (
 )
 
 const App = () => (
-  <ThemeProvider theme={theme}>
     <Router>
       <AppContainer>
         <Switch>
           <Route path="/" component={SamplePage} exact />
-          <Route path="/sample-page" component={AnotherSamplePage} />
+          <Route path="/sample-page" component={TestComp} />
           {/* <Route component={NotFoundPage} /> */}
         </Switch>
       </AppContainer>
     </Router>
-  </ThemeProvider>
 )
 export default App
