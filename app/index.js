32d066e5fc874a8cc41a0796349f03d9c3612b93
Bogdan Volyk <dekultism@gmail.com>
2018-01-02 18:25:03 +0200

    ♻️ Move to HashRouter
    BrowserRouter to HashRouter

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 // eslint-disable-next-line
 import { AppContainer } from 'react-hot-loader'
@@ -9,7 +9,7 @@ import routes from './routes'
 const render = () => {
   ReactDOM.render(
     <AppContainer>
-      <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+      <HashRouter>{renderRoutes(routes)}</HashRouter>
     </AppContainer>,
     document.getElementById('app'),
   )
