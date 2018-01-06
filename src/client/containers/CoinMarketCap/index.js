b02c0d025e9cec066b0b2eee2e9281e63b6d30dd
Bogdan Volyk <dekultism@gmail.com>
2018-01-06 18:42:12 +0200

    💩 Market!

--- src/client/containers/CoinMarketCap/index.js
+++ src/client/containers/CoinMarketCap/index.js
@@ -1,7 +1,60 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
+import { withStyles } from 'material-ui/styles'
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from 'material-ui/Table'
+import Paper from 'material-ui/Paper'
+import PropTypes from 'prop-types'
 
 import HomePage from 'components/pages/HomePage'
+
+const SampleList = props => {
+  const { classes, data } = props
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell numeric>Rank</TableCell>
+            <TableCell numeric>Name</TableCell>
+            <TableCell numeric>Market Cap</TableCell>
+            <TableCell numeric>Price</TableCell>
+            <TableCell numeric>Volume 24h</TableCell>
+            <TableCell numeric>Change 24h</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(n => (
+            <TableRow key={n.id}>
+              <TableCell>{n.rank}</TableCell>
+              <TableCell numeric>{n.name}</TableCell>
+              <TableCell numeric>{n.market_cap_usd}</TableCell>
+              <TableCell numeric>{n.price_usd}</TableCell>
+              <TableCell numeric>{n['24h_volume_usd']}</TableCell>
+              <TableCell numeric>{n.percent_change_24h}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  )
+}
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+})
+
+const StyledList = withStyles(styles)(SampleList)
 
 export default class CoinMarketCap extends Component {
   state = {
@@ -10,20 +63,36 @@ export default class CoinMarketCap extends Component {
     marketData: [],
   }
 
-  componentDidMount() {
-    console.log(123)
-  }
-
-  fetchMarketData = async () => {
-    const data = await fetch('https://api.coinmarketcap.com/v1/ticker/')
-    return await data.json()
+  async componentDidMount() {
+    // testing, move to HOC
+    this.setState({
+      loading: true,
+    })
+    const data = await fetch(
+      'https://api.coinmarketcap.com/v1/ticker/?limit=10',
+    )
+    const json = await data.json()
+    console.log(json)
+    this.setState({
+      loaded: true,
+      loading: false,
+    })
+    this.setState({
+      marketData: [...json],
+    })
   }
 
   render() {
+    const { marketData } = this.state
+    // console.log(this.state.marketData)
     return (
       <HomePage>
-        <div>zalupa</div>
+        <StyledList data={marketData} />
       </HomePage>
     )
   }
+}
+
+SampleList.propTypes = {
+  classes: PropTypes.object.isRequired,
 }
