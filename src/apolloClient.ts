f0fecc282ab716d614287969c4c38a9d27c43e3a
Bogdan Volyk <dekultism@gmail.com>
2018-02-03 07:00:25 +0200

    🚧 Apollo local state update

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,10 +1,17 @@
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import ApolloClient from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+import { withClientState } 'apollo-link-state'
+
 
 import { API_URL } from './config.ts'
 
+const cache = new InMemoryCache()
+const httpLink = new HttpLink({ uri: API_URL })
+const stateLink = withClientState({ resolvers, cache, defaults })
+
 export const client = new ApolloClient({
-  link: new HttpLink({ uri: API_URL }),
-  cache: new InMemoryCache(),
+  link: ApolloLink.from([stateLink, httpLink]),
+  cache,
 })
