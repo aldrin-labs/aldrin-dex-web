2b91622e757a13ba7f30cb971c8083eae8576285
Bogdan Volyk <dekultism@gmail.com>
2018-02-03 08:10:15 +0200

    ✨ Portfolio sidebar and main list

--- src/containers/Portfolio/Portfolio.tsx
+++ src/containers/Portfolio/Portfolio.tsx
@@ -1,13 +1,21 @@
 import React, { Fragment, SFC } from 'react'
+import styled from 'styled-components'
 
 import { NavBar } from '@components/NavBar'
 // import { PortfolioTable } from './PortfolioTable'
-// import { SelectAccount } from './SelectAccount'
+import { SelectAccount } from './SelectAccount'
+
+const SWrapper = styled.div`
+  display: flex;
+  margin-top: 5px;
+`
 
 export const Portfolio = () => (
   <Fragment>
     <NavBar />
+    <SWrapper>
     {/* <PortfolioTable /> */}
-    {/* <SelectAccount /> */}
+    <SelectAccount />
+  </SWrapper>
   </Fragment>
 )
