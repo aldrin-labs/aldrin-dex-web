83970163184e18fa527a79fe6e5ceaf6fba08ef1
Igor Limansky <i.e.limansky@gmail.com>
2018-02-05 15:01:32 +0300

    Merge

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,1 @@
-export const API_URL = 'https://api.graph.cool/simple/v1/ciwqxsma42cvh0125th9cn6q2'
-
-
+export const API_URL = 'http://api.igorlimansky.me/graphql'
--- src/containers/CoinMarketCap/CoinMarketCap.tsx
+++ src/containers/CoinMarketCap/CoinMarketCap.tsx
@@ -1,32 +1,82 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import { CoinsList } from './CoinsList'
 
-export class CoinMarketCap extends Component {
-  state = {
-    loading: false,
-    loaded: false,
-    marketData: [],
+const getCoinMarketCapQuery = gql`
+{
+	markets {
+	  id
+    rank
+    symbol
+    name
+    price_usd
+    market_cap_usd
+    total_supply
+    percent_change_24h
+	}
+}
+`
+
+class CoinMarketCap extends Component {
+
+  componentWillReceiveProps(newProps) {
+    if (!newProps.data.loading) {
+      if (this.subscription) {
+        if (newProps.data.markets !== this.props.data.markets) {
+          // if the feed has changed, we need to unsubscribe before resubscribing
+          this.subscription()
+        } else {
+          // we already have an active subscription with the right params
+          return
+        }
+      }
+      this.subscription = newProps
+        .data
+        .subscribeToMore({
+          document: gql `
+          subscription{
+            listenMarket(base: "USD"){
+            pair
+            pairId
+            price
+          }
+        } 
+        `, variables: null,
+          // this is where the magic happens.
+          updateQuery: (previousState, {subscriptionData}) => {
+            const newState = this.props.data.markets.map(x => ({...x}));
+            for (var i = 0; i< this.props.data.markets.length; ++ i){
+              if (newState[i].symbol === subscriptionData.data.listenMarket.pair.split('_')[0]){
+                console.log(newState[i].price_usd + ' === ' + subscriptionData.data.listenMarket.price);
+                newState[i].price_usd = subscriptionData.data.listenMarket.price;
+                console.log('update');
+                console.log(newState[i]);
+              }
+            }
+            const state = {markets: {}};
+            state.markets = newState;
+            return state;
+          }
+          ,
+          onError: (err) => console.error(err)
+        })
+    }
   }
 
-  async componentDidMount() {
-    // TODO: move to HOC
-    this.setState({
-      loading: true,
-    })
-    const data = await fetch('https://api.coinmarketcap.com/v1/ticker/?limit=25')
-    const json = await data.json()
-    this.setState({
-      loaded: true,
-      loading: false,
-    })
-    this.setState({
-      marketData: [...json],
-    })
-  }
 
   render() {
-    const { marketData } = this.state
-    return <CoinsList data={marketData} />
+    console.log(this.props.data);
+    return <div>
+      {
+        this.props.data.loading ?
+          <div />
+        :
+        <CoinsList data={this.props.data.markets} />
+      }
+    </div>
   }
 }
+
+export default graphql(getCoinMarketCapQuery, { options: {forceFetch: true }})(CoinMarketCap)
