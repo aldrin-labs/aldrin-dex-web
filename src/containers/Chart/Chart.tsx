af4507058d6fff171f45b182b0116fabf97c818e
Bogdan Volyk <dekultism@gmail.com>
2018-02-06 18:00:22 +0200

    🔀 merge dev with chart

---
+++ src/containers/Chart/Chart.tsx
@@ -0,0 +1,15 @@
+import { NavBar } from '@components/NavBar'
+import React, { SFC } from 'react'
+import styled from 'styled-components'
+
+const ChartContainer = styled.div`
+  height: 93vh;
+  width: 100%;
+`
+
+export const Chart = () => (
+    <ChartContainer>
+        <NavBar />
+        <iframe src={'http://localhost:3001'} height={'100%'} width={'100%'}/>
+    </ChartContainer>
+)
