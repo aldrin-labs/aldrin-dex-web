0fa1e2ef09dc1925c72bfc19ae26b7ed171aea40
Bogdan Volyk <dekultism@gmail.com>
2018-02-08 07:03:48 +0200

    🎨 Prettier fixes

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { Portfolio } from '@containers/Portfolio'
 import { Profile } from '@containers/Profile'
 import { Screener } from '@containers/Screener'
 import { client } from '@utils/apolloClient'
-import { store, persistor } from '@utils/configureStore'
+import { persistor, store } from '@utils/configureStore'
 
 import createHistory from 'history/createBrowserHistory'
 import React from 'react'
@@ -14,8 +14,8 @@ import { ApolloProvider } from 'react-apollo'
 import ReactDOM from 'react-dom'
 import { hot } from 'react-hot-loader'
 import { Provider } from 'react-redux'
+import { Route, Switch } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
-import { Route, Router, Switch } from 'react-router'
 import { PersistGate } from 'redux-persist/integration/react'
 
 const history = createHistory()
