63a0cdb90013dde53ad4ba17c36e0078812c5268
Bogdan Volyk <dekultism@gmail.com>
2018-02-08 07:02:16 +0200

    💥 Remove immutable
    persist + RRR5 works now

--- src/containers/Screener/Screener.tsx
+++ src/containers/Screener/Screener.tsx
@@ -1,6 +1,16 @@
 import React, { PureComponent, ReactNode } from 'react'
 import styled from 'styled-components'
+import { NavBar } from '@components/NavBar'
+import { compose } from 'recompose'
+import { connect } from 'react-redux'
 
-export const Screener = () => (
-  <div>Hello</div>
+export const ScreenerBase = () => (
+  <div>
+    <NavBar/>
+  </div>
 )
+
+export const Screener = compose(
+  connect(state => ({
+    count: console.log(state)
+})))(ScreenerBase)
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { Portfolio } from '@containers/Portfolio'
 import { Profile } from '@containers/Profile'
 import { Screener } from '@containers/Screener'
 import { client } from '@utils/apolloClient'
-import store from '@utils/configureStore'
+import { store, persistor } from '@utils/configureStore'
 
 import createHistory from 'history/createBrowserHistory'
 import React from 'react'
@@ -14,7 +14,9 @@ import { ApolloProvider } from 'react-apollo'
 import ReactDOM from 'react-dom'
 import { hot } from 'react-hot-loader'
 import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
 import { Route, Router, Switch } from 'react-router'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const history = createHistory()
 
@@ -22,7 +24,8 @@ const render = () =>
   ReactDOM.render(
     <ApolloProvider client={client}>
       <Provider store={store}>
-      <Router history={history}>
+      <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
         <Switch>
           <Route exact path="/" component={App} />
           <Route path="/profile" component={Profile} />
@@ -32,7 +35,8 @@ const render = () =>
           <Route path="/screener" component={Screener} />
           <Route path="*" component={NotFound} />
         </Switch>
-      </Router>
+      </ConnectedRouter>
+      </PersistGate>
       </Provider>
     </ApolloProvider>,
     document.getElementById('root'),
