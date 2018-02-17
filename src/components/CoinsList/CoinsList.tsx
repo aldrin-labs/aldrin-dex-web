eadf7de998c0f6794a11c06a7e323066cc469b6a
Bogdan Volyk <dekultism@gmail.com>
2018-02-17 03:34:32 +0200

    :construction: Split container to components

--- src/components/CoinsList/CoinsList.tsx
+++ src/components/CoinsList/CoinsList.tsx
@@ -1,3 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import nanoid from 'nanoid'
 
+import Paper from 'material-ui/Paper'
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table'
+
+import * as T from './types'
+
+const CoinListPaper = styled(Paper)`
+  width: 100%;
+  margin-top: 5px;
+  overflow-x: auto;
+`
+
+const CoinsListTable = styled(Table)`
+  min-width: 700px;
+  max-width: 1100px;
+  margin: 0 auto;
+`
+
+export const CoinsListHead = ({ tableRows }: T.ICoinsTableList[]) => (
+  <TableHead>
+    {tableRows.map((row: T.ICoinsTableCell) => (
+      <TableCell key={nanoid} numeric>
+        {row}
+      </TableCell>
+    ))}
+  </TableHead>
+)
---
+++ src/components/CoinsList/types.ts
@@ -0,0 +1,8 @@
+export interface ICoinsTableCell {
+  row: string
+}
+
+export interface ICoinsTableList {
+  tableRows: ICoinsTableCell[]
+}
+
