b96f7b446688c7a2a2e36cb0e933a50b24fa4b94
Bogdan Volyk <dekultism@gmail.com>
2018-02-22 16:11:28 +0200

    :art: redux-observable logic

--- src/containers/User/User.tsx
+++ src/containers/User/User.tsx
@@ -1,4 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
+import * as actions from './actions'
+console.log(actions.addTodo.getType())
 
-export const User = () => <div>Hello</div>
+const UserComp = (props) => {
+  console.log(111, props)
+return <div>Hello <button onClick={props.addTodo}>Checing </button></div>
+}
+
+function mapStateToProps (state) {
+  return {
+    check: state.user.check
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    addTodo: () => dispatch(actions.addTodo())
+  }
+}
+
+export const User = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserComp)
--- src/utils/rootEpic.ts
+++ src/utils/rootEpic.ts
@@ -1,7 +1,9 @@
 import { combineEpics, Epic } from 'redux-observable'
 
 import ScreenerEpics from '@containers/Screener/epics'
+import UserEpics from '@containers/User/epics'
 
 export default combineEpics(
-  ScreenerEpics
+  ScreenerEpics,
+  UserEpics
 )
