f54e0aae480b0b92797cc608fc9dde00c4b8273c
Bogdan Volyk <dekultism@gmail.com>
2018-02-27 06:37:52 +0200

    :bug: Gql fix

--- src/containers/Settings/KeysList.tsx
+++ src/containers/Settings/KeysList.tsx
@@ -1,40 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
-import { compose } from 'recompose'
+import { compose, branch, renderComponent, withProps } from 'recompose'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
+import { propType } from 'graphql-anywhere'
 
 import { Loading } from '@components/Loading'
 import { withErrorFallback } from '@hoc'
 
 const getKeys = gql`
   query getKeys {
-  getProfile {
-    username
-    imageUrl
-    email
-    emailSubscription
-    portfolioId
-    keys {
-      name
-      date
-      credentials {
-        public
-        secret
-      }
-      exchange {
+    getProfile {
+      username
+      imageUrl
+      email
+      emailSubscription
+      portfolioId
+      keys {
         name
-        symbol
+        date
+        credentials {
+          public
+          secret
+        }
+        exchange {
+          name
+          symbol
+        }
       }
     }
   }
-}
 `
 
-const Keys = (props) => (
-  <div>{console.log(props)}</div>
+const renderWhileLoading = (component, propName = 'data') =>
+  branch(props => props[propName] && props[propName].loading, renderComponent(component))
+
+const renderForError = (component, propName = 'data') =>
+  branch(props => props[propName] && props[propName].error, renderComponent(component))
+
+const Error = props => (
+  <span>
+    Something went wrong try to <button onClick={props.refetch}>refetch</button>
+  </span>
 )
+
+const setRefetchProp = (propName = 'data') =>
+  withProps(props => ({ refetch: props[propName] && props[propName].data }))
+
+const Keys = props => <div>{console.log(props)}</div>
 
 export const KeysList = compose(
   graphql(getKeys, { name: 'keysList' }),
+  renderWhileLoading(Loading, 'keysList'),
+  setRefetchProp('keysList'),
+  renderForError(Error, 'keysList')
 )(Keys)
