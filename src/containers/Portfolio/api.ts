0da9e7ff502a2ade7276639e8d28984544592809
Bogdan Volyk <dekultism@gmail.com>
2018-03-11 15:17:16 +0200

    :wrench: GraphQL import ts fix

--- src/containers/Portfolio/api.ts
+++ src/containers/Portfolio/api.ts
@@ -1,9 +1,17 @@
 import gql from 'graphql-tag'
+import getProfileQuery from './graphql/GetProfile.gql'
+
 
 export const getPortfolio = gql`
   query gerPortfolio {
     getProfile {
       username
       portfolioId
+      keyIds
+      keys {
+        name
+        apiKey
+
+      }
   }
 `
