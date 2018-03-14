93eb7958d76028a1188ad0e1c6883c7e39579cbf
Bogdan Volyk <dekultism@gmail.com>
2018-03-15 00:47:32 +0200

    :construction: Auth component for JWT check fix

--- src/containers/Login/epics.ts
+++ src/containers/Login/epics.ts
@@ -4,10 +4,10 @@ import * as actions from './actions'
 function storeLogin(profile: any) {
   return {
     type: actions.storeLogin,
-    payload: profile
+    payload: profile,
   }
 }
 
 export const lalka = (action$: any) =>
   action$.ofType(actions.onLogin)
-    .map(storeLogin)
+  .map(storeLogin)
