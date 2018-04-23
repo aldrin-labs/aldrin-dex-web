be051bb2ee6c90e90bde163296c232f06fe3a7b0
avernikoz <avernikoz@gmail.com>
2018-04-23 14:38:21 +0300

    decimals with 2 digits after point works, but need to delete test information in future commit

--- src/containers/Portfolio/components/PortfolioTable/PortfolioTableSum.tsx
+++ src/containers/Portfolio/components/PortfolioTable/PortfolioTableSum.tsx
@@ -6,14 +6,46 @@ import selectedIcon from '../../../../icons/selected.svg'
 interface Props {
   selectedSum: {
     [key: string]: string | number
-  }
+  },
+  isUSDCurrently: boolean
 }
 
 export default class PortfolioTableSum extends React.Component<Props> {
   onFloorN = (x: number, n: number) => {
-    if (typeof x === 'string') return x
-    var mult = Math.pow(10, n)
-    return Math.floor(x * mult) / mult
+    if (typeof x === 'string') return [x,x];
+
+    const reg = this.props.isUSDCurrently
+      ? /-?[0-9]+(?=\.[0-9]+)\.[0-9]{2}/g
+      : /-?[0-9]+(?=\.[0-9]+)\.[0-9]{8}/g
+
+
+    var mult = Math.pow(10, n);
+    let multAfterCalculation = Math.floor(x * mult) / mult;
+
+    let price;
+    if (multAfterCalculation.toString().match(reg)) {
+      price = multAfterCalculation.toString().match(reg);
+      return [price[0],multAfterCalculation];
+    }
+    else if (multAfterCalculation) {
+      return [multAfterCalculation,multAfterCalculation];
+    }
+    else {
+      return ['0', multAfterCalculation];
+    }
+
+    // price = price.match(reg);
+    console.log(typeof price[0]);
+
+    console.log('this is price: ' + price[0]);
+    console.log('this is mult: ' + multAfterCalculation);
+    console.log('this is mult with toString: ' + multAfterCalculation.toString());
+
+
+
+    // return Math.floor(x * mult) / mult
+    // console.log(this.props.isUSDCurrently);
+    // return
   }
 
   render() {
@@ -31,7 +63,8 @@ export default class PortfolioTableSum extends React.Component<Props> {
             if (!Number.isNaN(selectedSum[key])) {
               res = this.onFloorN(selectedSum[key], 3)
             }
-            return <PTD key={key}>{res || ''}</PTD>
+            // return <PTD key={key}>{res || ''}</PTD>
+            return <PTD key={key}>{res[0] + '   ' + res[1] || ''}</PTD>
           })}
         </PTR>
       </PTBody>
