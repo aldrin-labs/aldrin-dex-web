c5bd0edb84b3db2496f7b74a5a503654274c6c57
avernikoz <avernikoz@gmail.com>
2018-04-25 19:11:02 +0300

    add processing condition to spinner

--- src/containers/Portfolio/components/PortfolioTable/PortfolioTable.tsx
+++ src/containers/Portfolio/components/PortfolioTable/PortfolioTable.tsx
@@ -353,20 +353,24 @@ export class PortfolioTable extends React.Component<TableProps> {
     // if (!this.props.data) return null
 
     if (this.props.data && !tableData) {
+
+      if (this.props.data.processing) {
+
       return (
-        <LoaderWrapper>
-          <SvgIcon
-            src={spinLoader}
-            width={48}
-            height={48}
-            style={{
-              position: 'absolute',
-              left: 'calc(50% - 48px)',
-              top: 'calc(50% - 48px)',
-            }}
-          />
-        </LoaderWrapper>
-      )
+          <LoaderWrapper>
+            <SvgIcon
+              src={spinLoader}
+              width={48}
+              height={48}
+              style={{
+                position: 'absolute',
+                left: 'calc(50% - 48px)',
+                top: 'calc(50% - 48px)',
+              }}
+            />
+          </LoaderWrapper>
+        )
+      }
     }
 
     const isSelectAll =
