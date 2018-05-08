de82d82f3276d334b6ce59670290f6324f8ae3f9
avernikoz <avernikoz@gmail.com>
2018-05-08 14:14:56 +0300

    Moved Correlation mocks & moved some Correlation functions to utils

--- src/containers/Portfolio/components/PortfolioTable/Correlation/Correlation.tsx
+++ src/containers/Portfolio/components/PortfolioTable/Correlation/Correlation.tsx
@@ -1,104 +1,16 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import HeatMapChart from '@components/HeatMapChart'
-import CORRELATION_MOCKS from 'utils/corr_matrices_total.json'
+import { HeatMapMocks } from './mocks'
 import { onFloorN } from '../../../../../utils/PortfolioTableUtils'
+import {
+  getColor,
+  getHeatMapData,
+  optimizeMocks,
+} from '../../../../../utils/PortfolioCorrelationUtils'
+import { IProps } from './Correlation.types'
 
-function optimizeMocks(): { rows: string[]; cols: any[][] } {
-  const m = JSON.parse(CORRELATION_MOCKS['2018-04-24'])
-  const o = Object.keys(m).map((key) => ({ [key]: m[key] }))
-  const rows = o.map((a) => {
-    const arr = Object.keys(a)
-
-    return arr[0].slice(0, -7)
-  })
-
-  const cols = o.map((a) =>
-    Object.keys(a).map((key) => {
-      const s = a[key]
-
-      return Object.keys(s).map((keyV) => s[keyV])
-    })
-  )
-
-  return { rows, cols }
-}
-
-optimizeMocks()
-
-const HeatMapMocks = [
-  { pair: 'BTC/ETH', values: [{ v: '0.87' }, { v: '0.54' }] },
-  { pair: 'BTC/XRP', values: [{ v: '0.26' }, { v: '0.48' }] },
-  { pair: 'ETH/XRP', values: [{ v: '0.62' }, { v: '0.89' }] },
-  { pair: 'BTC/BCH', values: [{ v: '1' }, { v: '0.88' }] },
-  { pair: 'ETH/BCH', values: [{ v: '-0.02' }, { v: '0.14' }] },
-  { pair: 'XRP/BCH', values: [{ v: '-0.56' }, { v: '-0.94' }] },
-  { pair: 'BTC/LTC', values: [{ v: '0.01' }, { v: '-0.98' }] },
-]
-
-function getColor(value: string): string {
-  const n = Number(value)
-  if (n >= 0.9) {
-    return '#1B5E20'
-  } else if (n >= 0.8 && n < 0.9) {
-    return '#2E7D32'
-  } else if (n >= 0.7 && n < 0.8) {
-    return '#388E3C'
-  } else if (n >= 0.6 && n < 0.7) {
-    return '#43A047'
-  } else if (n >= 0.5 && n < 0.6) {
-    return '#4CAF50'
-  } else if (n >= 0.4 && n < 0.5) {
-    return '#66BB6A'
-  } else if (n >= 0.3 && n < 0.4) {
-    return '#81C784'
-  } else if (n >= 0.2 && n < 0.3) {
-    return '#A5D6A7'
-  } else if (n >= 0.1 && n < 0.2) {
-    return '#C8E6C9'
-  } else if (n >= 0 && n < 0.1) {
-    return '#E8F5E9'
-  } else if (n >= -0.1 && n < 0) {
-    return '#B9F6CA'
-  } else if (n >= -0.2 && n < -0.1) {
-    return '#FFEBEE'
-  } else if (n >= -0.3 && n < -0.2) {
-    return '#FFCDD2'
-  } else if (n >= -0.4 && n < -0.3) {
-    return '#EF9A9A'
-  } else if (n >= -0.5 && n < -0.4) {
-    return '#E57373'
-  } else if (n >= -0.6 && n < -0.5) {
-    return '#EF5350'
-  } else if (n >= -0.7 && n < -0.6) {
-    return '#F44336'
-  } else if (n >= -0.8 && n < -0.7) {
-    return '#E53935'
-  } else if (n >= -0.9 && n < -0.8) {
-    return '#D32F2F'
-  } else if (n >= -1 && n < -0.9) {
-    return '#C62828'
-  }
-}
-
-function getHeatMapData(
-  data: { pair: string; values: { [key: string]: string }[] }[]
-) {
-  const result: { x: number; y: number }[] = []
-  data.forEach((item, i) => {
-    item.values.forEach((value, idx) => {
-      result.push({
-        x: i + 1,
-        y: idx * 2,
-        color: getColor(value.v),
-      })
-    })
-  })
-
-  return result
-}
-
-export default class Correlation extends React.Component {
+export default class Correlation extends React.Component<IProps> {
   initializeArray = (length: number, start: number, step: number): number[] =>
     Array.from({ length: Math.ceil((length - start) / step + 1) }).map(
       (v, i) => i * step + start
@@ -109,7 +21,7 @@ export default class Correlation extends React.Component {
     const { cols, rows } = optimizeMocks()
 
     return (
-      <PTWrapper>
+      <PTWrapper data={!!cols.length && !!rows.length}>
         {children}
         <Wrapper>
           <ScrolledWrapper>
@@ -144,7 +56,7 @@ export default class Correlation extends React.Component {
                       </Item>
                     )}
                     {col.map((el) =>
-                      el.map((e) => {
+                      el.map((e: string) => {
                         const value = onFloorN(Number(e), 2)
                         const color = getColor(e)
 
@@ -173,7 +85,8 @@ export default class Correlation extends React.Component {
 }
 
 const PTWrapper = styled.div`
-  width: calc(100% - 240px);
+  width: ${(props: { data?: boolean }) =>
+    props.data ? 'calc(100% - 240px);' : '100%'};
   display: flex;
   flex-direction: column;
   margin: 24px;
