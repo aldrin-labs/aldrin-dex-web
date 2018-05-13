ca6a96d10f4c0c2fba9c7798f0c9cb8cf2bf38c6
avernikoz <avernikoz@gmail.com>
2018-05-13 21:33:21 +0300

    all .ts files fixed errors

--- src/containers/Portfolio/reducer.ts
+++ src/containers/Portfolio/reducer.ts
@@ -12,18 +12,22 @@ export default createReducer(
   {
     [actions.getPortfolio]: (state, payload) => {
       console.log(3333, state, payload)
+
       return { ...state, ...payload }
     },
     [actions.updateSelectedAccounts]: (state, payload) => {
       console.log(11111, state, payload)
-      return {...state, selectedAccounts: [...payload] }
+
+      return { ...state, selectedAccounts: [...payload] }
     },
     [actions.selectAllKeys]: (state, payload) => {
       const isEqual = R.equals(state.selectedAccounts, payload)
       if (isEqual) {
-        return {...state, selectedAccounts: []}
+        return { ...state, selectedAccounts: [] }
       }
-      return {...state, selectedAccounts: [].concat(payload)}
-    }
-  }, initialState
+
+      return { ...state, selectedAccounts: [].concat(payload) }
+    },
+  },
+  initialState
 )
