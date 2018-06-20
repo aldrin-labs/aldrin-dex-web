9fae6e257ab0c6cfec346c92f277e584f019e7e4
avernikoz <avernikoz@gmail.com>
2018-06-20 14:55:00 +0300

    added interfaces to Selector

--- src/containers/Screener/Selector.types.ts
+++ src/containers/Screener/Selector.types.ts
@@ -10,6 +10,23 @@ export interface IState {
   price: string
   peg: string
   volume: string
+  performance: string,
+  performance2: string,
+  twenty20DayHighLow: string,
+  twenty20DaySimpleMoving: string,
+  fifty50DaySimpleMoving: string,
+  fifty50DayHighLow: string,
+  averageTrueRange: string,
+  twoHundreds200SimpleMoving: string,
+  fiftyTwo52WeekHighLow: string,
+  rsi14: string,
+  change: string,
+  pattern: string,
+  gap: string,
+  changeFromOpen: string,
+  candleStick: string,
+  volatility: string,
+
   data: {
     coin: { label: string; value: string }[]
     marketCap: { label: string; value: string }[]
