06bbdec64ea3217ee391f15f37e71af5e754faa9
addamove <empire.empo@gmail.com>
2018-06-25 14:17:54 +0300

    Fixed(clicking on plus button in chart page highlights some text)

--- src/containers/Chart/Tables/Aggregation/Aggregation.tsx
+++ src/containers/Chart/Tables/Aggregation/Aggregation.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { MdAddCircleOutline } from 'react-icons/lib/md/'
+import styled from 'styled-components'
+
 import { Row, Table, Head, Cell, HeadCell } from '@components/Table/Table'
 
 const Aggregation = (props) => {
@@ -37,16 +39,43 @@ const Aggregation = (props) => {
             color="#9ca2aa"
             width={'25%'}
           >
-            <MdAddCircleOutline
-              onClick={onButtonClick}
-              style={{ fontSize: '1rem', cursor: 'pointer' }}
-            />
+            <Button>
+              <MdAddCircleOutline
+                onClick={onButtonClick}
+                style={{
+                  fontSize: '0.75rem',
+                  cursor: 'pointer',
+                  position: 'relative',
+                }}
+              />
+            </Button>
           </HeadCell>
         </Row>
       </Head>
     </AggregationWrapper>
   )
 }
+
+const Button = styled.button`
+  position: relative;
+  bottom: 1px;
+
+  border: none;
+  margin: 0;
+  padding: 0;
+  width: auto;
+  overflow: visible;
+  text-align: inherit;
+  color: white;
+  background: transparent;
+
+  line-height: normal;
+
+  &::-moz-focus-inner {
+    border: 0;
+    padding: 0;
+  }
+`
 
 const AggregationWrapper = Table.extend`
   @media (max-width: 1080px) {
