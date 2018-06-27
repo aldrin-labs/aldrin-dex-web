185cd620a2b193500144164fffe6c638ab84c1db
avernikoz <avernikoz@gmail.com>
2018-06-27 16:50:31 +0300

    typescript Selector-interfaces updated

--- src/containers/Screener/Selector/Selector.types.ts
+++ src/containers/Screener/Selector/Selector.types.ts
@@ -14,38 +14,17 @@ export interface IState {
   closingPriceAverage: string
   averageVolume: string
   averageVolumeOnBalance: string
-  // chaikinMoneyFlow: string
+  low: string
+  high: string
 
-  one1DayLow: string
-  one1DayHigh: string
-  one1HourLow: string
-  one1HourHigh: string
-  one1MonthLow: string
-  one1MonthHigh: string
-  three3MonthLow: string
-  three3MonthHigh: string
-  twelve12MonthLow: string
-  twelve12MonthHigh: string
-
-  InputMarketCap: string
-  InputChangeInDigits: string
-  InputChangeInPercentage: string
-  InputSimpleMovingAverage: string
-  InputClosingPriceAverage: string
-  InputAverageVolume: string
-  InputAverageVolumeOnBalance: string
-  InputChaikinMoneyFlow: string
-
-  InputOne1HourLow: string
-  InputOne1HourHigh: string
-  InputOne1DayLow: string
-  InputOne1DayHigh: string
-  InputOne1MonthLow: string
-  InputOne1MonthHigh: string
-  InputThree3MonthLow: string
-  InputThree3MonthHigh: string
-  InputTwelve12MonthLow: string
-  InputTwelve12MonthHigh: string
+  marketCapInput: string
+  changeInPercentageInput: string
+  simpleMovingAverageInput: string
+  closingPriceAverageInput: string
+  averageVolumeInput: string
+  averageVolumeOnBalanceInput: string
+  lowInput: string
+  highInput: string
 
   showFilters: boolean
 }
