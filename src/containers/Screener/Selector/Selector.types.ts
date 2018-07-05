db427619959a9ee39e735a48883ce64b8161a551
avernikoz <avernikoz@gmail.com>
2018-07-05 12:39:07 +0300

    Selector.types changed

--- src/containers/Screener/Selector/Selector.types.ts
+++ src/containers/Screener/Selector/Selector.types.ts
@@ -8,7 +8,6 @@ export interface IState {
   timeInterval: string
   industry: undefined[] | string[]
 
-  marketCap: string
   marketCapSlider: string
   changeInPercentage: string
   simpleMovingAverage: string
@@ -18,7 +17,6 @@ export interface IState {
   low: string
   high: string
 
-  marketCapInput: string
   changeInPercentageInput: string
   simpleMovingAverageInput: string
   closingPriceAverageInput: string
