6e56de1836b648498dd4deb0a5c59847baa8428a
addamove <empire.empo@gmail.com>
2018-07-06 21:49:01 +0300

    fixed arrow

--- src/containers/Chart/Inputs/AutoSuggestSelect/AutoSuggestSelect.tsx
+++ src/containers/Chart/Inputs/AutoSuggestSelect/AutoSuggestSelect.tsx
@@ -46,7 +46,7 @@ class Option extends React.Component {
       openWarningMessage,
       removeWarningMessage,
     } = this.props
-    console.log('here')
+
     onSelect(this.props.option, event)
     if (view === 'default') {
       selectCurrencies(this.props.option.value)
@@ -119,7 +119,7 @@ function SelectWrapped(props) {
       optionComponent={Opt}
       noResultsText={<Typography>{'No results found'}</Typography>}
       arrowRenderer={(arrowProps) =>
-        arrowProps.isOpen ? <MdArrowDropDown /> : <MdArrowDropUp />
+        arrowProps.isOpen ? <MdArrowDropUp /> : <MdArrowDropDown />
       }
       clearRenderer={() => <MdClear />}
       valueComponent={(valueProps) => {
