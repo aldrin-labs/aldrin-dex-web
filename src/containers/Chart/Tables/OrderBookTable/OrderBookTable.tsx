be854d816318463bdd5dd774dc042032e861d52a
Igor Limansky <i.e.limansky@gmail.com>
2018-07-18 14:49:57 +0300

    hook up orders to api real-time data

--- src/containers/Chart/Tables/OrderBookTable/OrderBookTable.tsx
+++ src/containers/Chart/Tables/OrderBookTable/OrderBookTable.tsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import styled, { keyframes } from 'styled-components'
 import { Button } from '@material-ui/core'
+import { Query, Subscription } from 'react-apollo'
 
 import {
   Table,
@@ -13,10 +14,77 @@ import {
 } from '@components/Table/Table'
 import AnimatedCell from '@components/Table/AnimatedCell/AnimatedCell'
 import { Loading } from '@components/Loading/Loading'
+import gql from 'graphql-tag'
+
+export const MARKET_ORDERS = gql`
+  subscription listenMarketOrders($symbol: String!, $exchange: String!) {
+    listenMarketOrders(symbol: $symbol, exchange: $exchange)
+  }
+`
+
+export const MARKET_QUERY = gql`
+  query marketOrders {
+    marketOrders
+  }
+`
+
+class OrdersList extends React.Component {
+  state = {
+    data: []
+  }
+  componentWillMount() {
+    this.props.subscribeToNewComments();
+  }
+  componentWillReceiveProps(newProps) {
+    if (newProps.data && newProps.data.marketOrders && newProps.data.marketOrders.length > 0) {
+      const order = JSON.parse(newProps.data.marketOrders[0]);
+      this.setState((prevState, props) => ({ data: [order, ...prevState.data] }));
+      // if (!newProps.query.loading) {
+      //  this.setState({ data: newProps.query.data })
+      // }
+    }
+  }
+  render() {
+    return (
+      <div>
+        {
+          this.state.data.map((order, i) => (
+            <Row key={i} background={'#292d31'}>
+              <EmptyCell
+                status={'rise'}
+                colored={order.percentageOfChange ? order.percentageOfChange.toString() : "0"}
+                color="#9ca2aa"
+                width={'25%'}
+              />
+
+              <AnimatedCell
+                value={Number(order.size).toFixed(8)}
+                color="#9ca2aa"
+                animation={'fadeInGreenAndBack'}
+                width={'35%'}
+              />
+              <AnimatedCell
+                value={this.props.roundTill(
+                  this.props.aggregation,
+                  Number(order.price).toFixed(2)
+                ).toFixed(2)}
+                animation={'fadeInGreen'}
+                color="#34cb86d1"
+                width={'30%'}
+              />
+            </Row>
+          ))
+        }
+      </div>
+    );
+  };
+}
 
 class OrderBookTable extends PureComponent {
   render() {
     const { onButtonClick, roundTill, aggregation, quote, data } = this.props
+    const symbol = this.props.symbol ? this.props.symbol : 'ETH_BTC';
+    const exchange = this.props.exchange ? this.props.exchange : 'gateio';
 
     if (!data) {
       return <Loading centerAligned />
@@ -61,32 +129,33 @@ class OrderBookTable extends PureComponent {
           </Row>
         </Head>
         <Body height={'calc(100vh - 59px - 80px - 39px - 37px - 24px - 26px)'}>
-          {data.map((order, i) => (
-            <Row key={i} background={'#292d31'}>
-              <EmptyCell
-                status={'rise'}
-                colored={order.percentageOfChange.toString()}
-                color="#9ca2aa"
-                width={'25%'}
-              />
-
-              <AnimatedCell
-                value={Number(order.size).toFixed(8)}
-                color="#9ca2aa"
-                animation={'fadeInGreenAndBack'}
-                width={'35%'}
-              />
-              <AnimatedCell
-                value={roundTill(
-                  aggregation,
-                  Number(order.price).toFixed(2)
-                ).toFixed(2)}
-                animation={'fadeInGreen'}
-                color="#34cb86d1"
-                width={'30%'}
-              />
-            </Row>
-          ))}
+          <Query
+            query={MARKET_QUERY}
+          >
+            {({ subscribeToMore, ...result }) =>
+              (
+                <OrdersList
+                  roundTill={roundTill}
+                  aggregation={aggregation}
+                  {...result}
+                  subscribeToNewComments={() =>
+                    subscribeToMore({
+                      document: MARKET_ORDERS,
+                      variables: { symbol, exchange },
+                      updateQuery: (prev, { subscriptionData }) => {
+                        if (!subscriptionData.data) { return prev; }
+                        const newFeedItem = subscriptionData.data.listenMarketOrders;
+                        let obj = Object.assign({}, prev, {
+                          marketOrders: [newFeedItem]
+                        });
+                        console.log(obj);
+                        return obj;
+                      }
+                    })
+                  }
+                />
+              )}
+          </Query>
         </Body>
       </Table>
     )
@@ -97,39 +166,40 @@ const SwitchTablesButton = styled(Button)`
   && {
     display: none;
 
-    @media (max-width: 1080px) {
-      display: block;
-    }
+    @media(max - width: 1080px) {
+  display: block;
+}
   }
 `
 
 const fadeInGreen = keyframes`
-0% {
+0 % {
   color: #9ca2aa;
 }
-50% {
+50 % {
   color: #34cb86d1;
 }
-100% {
+100 % {
   color: #9ca2aa;
 }
-`
+  `
 const fadeInRed = keyframes`
-0% {
+0 % {
   color: #9ca2aa;
 }
-50% {
+50 % {
   color: #d77455;
 
 }
-100% {
+100 % {
   color: #9ca2aa;
 
 }
-`
+  `
 
 const Cell = styled(RowCell)`
-  animation: ${(props: { animated?: string; width: string; color: string }) => {
+animation: ${
+  (props: { animated?: string; width: string; color: string }) => {
     if (props.animated === 'none') {
       return ''
     }
@@ -143,24 +213,27 @@ const Cell = styled(RowCell)`
     }
 
     return ''
-  }};
+  }
+  };
 `
 
 const EmptyCell = Cell.extend`
-  position: relative;
+position: relative;
 
-  &:before {
-    transition: all 0.5s linear;
-    position: absolute;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    width: ${(props: { colored?: string }) => Number(props.colored) / 4}%;
-    height: 100%;
-    content: '';
-    background-color: ${(props: { status?: string; colored?: string }) =>
-      props.status === 'fall' ? '#d77455' : '#34cb86d1'};
-  }
+  &: before {
+  transition: all 0.5s linear;
+  position: absolute;
+  z - index: 100;
+  top: 0;
+  left: 0;
+  width: ${ (props: { colored?: string }) => Number(props.colored) / 4}%;
+  height: 100 %;
+  content: '';
+  background - color: ${
+  (props: { status?: string; colored?: string }) =>
+    props.status === 'fall' ? '#d77455' : '#34cb86d1'
+  };
+}
 `
 
 export default OrderBookTable
