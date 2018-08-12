fad7a0482b2337a2f3fb7a4a62a4715800ef9d7d
avernikoz <avernikoz@gmail.com>
2018-08-12 18:10:26 +0300

    Added types generated from apollo-cli

--- src/containers/Portfolio/components/PortfolioTable/Rebalance/PortfolioTableRebalance.types.ts
+++ src/containers/Portfolio/components/PortfolioTable/Rebalance/PortfolioTableRebalance.types.ts
@@ -16,6 +16,9 @@ export interface IProps {
   isUSDCurrently: boolean
   isShownMocks: boolean
   filterValueSmallerThenPercentage: number
+  getMyRebalance: IGetMyRebalanceQuery
+  getMyPortfolio: IGetMyPortfolioQuery
+  updateRebalanceMutationQuery: Function
 }
 
 export interface IState {
@@ -56,6 +59,115 @@ export interface IShapeOfCurrentPortolioRow {
   asset: { symbol: string; priceUSD: string }
 }
 
-export interface IGetMyRebalanceQuery {
 
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: IGetMyRebalanceQuery
+// ====================================================
+
+export interface IGetMyRebalanceQuery_getProfile_myRebalance_assets {
+  id: any | null;
+  _id: any | null;
+  percent: any | null;
+  amount: any | null;
+  diff: any | null;
+}
+
+export interface IGetMyRebalanceQuery_getProfile_myRebalance {
+  total: any | null;
+  assets: (IGetMyRebalanceQuery_getProfile_myRebalance_assets | null)[] | null;
+}
+
+export interface IGetMyRebalanceQuery_getProfile {
+  myRebalance: IGetMyRebalanceQuery_getProfile_myRebalance | null;
+}
+
+export interface IGetMyRebalanceQuery {
+  getProfile: IGetMyRebalanceQuery_getProfile | null;
+}
+
+
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: IGetMyPortfolioQuery
+// ====================================================
+
+export interface IGetMyPortfolioQuery_getProfile_portfolio_assets_exchange {
+  name: string | null;
+}
+
+export interface IGetMyPortfolioQuery_getProfile_portfolio_assets_asset {
+  symbol: string | null;
+  priceUSD: string | null;
+}
+
+export interface IGetMyPortfolioQuery_getProfile_portfolio_assets {
+  value: number | null;
+  exchange: IGetMyPortfolioQuery_getProfile_portfolio_assets_exchange | null;
+  asset: IGetMyPortfolioQuery_getProfile_portfolio_assets_asset | null;
+}
+
+export interface IGetMyPortfolioQuery_getProfile_portfolio {
+  assetIds: (any | null)[] | null;
+  assets: (IGetMyPortfolioQuery_getProfile_portfolio_assets | null)[] | null;
+}
+
+export interface IGetMyPortfolioQuery_getProfile {
+  portfolio: IGetMyPortfolioQuery_getProfile_portfolio | null;
+}
+
+export interface IGetMyPortfolioQuery {
+  getProfile: IGetMyPortfolioQuery_getProfile | null;
+}
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export interface rebalanceInput {
+  assets?: assetList | null;
+  total?: string | null;
+}
+
+export interface assetList {
+  input?: (asset | null)[] | null;
+}
+
+export interface asset {
+  _id?: _id | null;
+  percent?: string | null;
+  amount?: string | null;
+  diff?: string | null;
+}
+
+export interface _id {
+  exchange?: string | null;
+  coin?: string | null;
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
+
+
+
+// ====================================================
+// GraphQL mutation operation: IUpdateRebalanceMutation
+// ====================================================
+
+export interface IUpdateRebalanceMutation_updateRebalance {
+  total: any | null;
+}
+
+export interface IUpdateRebalanceMutation {
+  updateRebalance: IUpdateRebalanceMutation_updateRebalance | null;
+}
+
+export interface IUpdateRebalanceMutationVariables {
+  input?: rebalanceInput | null;
 }
