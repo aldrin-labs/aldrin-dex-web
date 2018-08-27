451a63445f41a3159644c01eef9024e2debed5ef
addamove <empire.empo@gmail.com>
2018-08-27 12:50:04 +0300

    added new vloume layot

--- src/containers/Chart/Tables/OrderBookTable/Tables/Asks/OrderBookBody/OrderBookBody.tsx
+++ src/containers/Chart/Tables/OrderBookTable/Tables/Asks/OrderBookBody/OrderBookBody.tsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import { red } from '@material-ui/core/colors'
 
 import { Row, Body, Cell } from '@components/Table/Table'
-import { TypographyFullWidth } from '@utils/cssUtils'
-import { opacityAnimation } from '../../../../../../../styles/keyframes'
 import { Loading } from '@components/Loading'
 import { calculatePercentagesOfOrderSize } from '@utils/chartPageUtils'
-import { EmptyCell } from '@containers/Chart/Tables/OrderBookTable/Tables/Asks/SharedStyles'
 import { IProps } from './OrderBookBody.types'
+import {
+  EmptyCell,
+  RowWithVolumeChart,
+  StyledTypography,
+} from '@containers/Chart/Tables/SharedStyles'
+import { hexToRgbAWithOpacity } from '../../../../../../../styles/helpers'
 
 class ClassBody extends Component<IProps> {
   componentDidMount() {
@@ -40,47 +43,54 @@ class ClassBody extends Component<IProps> {
           <Loading centerAligned={true} />
         ) : (
           <>
-            {data.map((order, i) => (
-              <Row
-                hoverBackground={action.hover}
-                key={i}
-                background={background.default}
-              >
-                <EmptyCell
-                  colored={calculatePercentagesOfOrderSize(
-                    +order.size,
-                    data
-                  ).toString()}
-                  width={'10%'}
-                />
-                <Cell width={'45%'}>
-                  <StyledTypography
-                    anime={i === index}
-                    textColor={red[400]}
-                    color="default"
-                    noWrap={true}
-                    variant="body1"
-                    align="right"
+            {data.map(
+              (
+                order: { size: number | string; price: number | string },
+                i: number
+              ) => (
+                <Row background={'transparent'} key={order.price}>
+                  <RowWithVolumeChart
+                    volumeColor={hexToRgbAWithOpacity(red[500], 0.25)}
+                    colored={calculatePercentagesOfOrderSize(
+                      +order.size,
+                      data
+                    ).toString()}
+                    hoverBackground={action.hover}
+                    background={background.default}
                   >
-                    {Number(order.size).toFixed(digitsAfterDecimalForAsksSize)}
-                  </StyledTypography>
-                </Cell>
-                <Cell width={'45%'}>
-                  <StyledTypography
-                    anime={i === index}
-                    textColor={red[400]}
-                    color="default"
-                    noWrap={true}
-                    variant="body1"
-                    align="right"
-                  >
-                    {Number(order.price).toFixed(
-                      digitsAfterDecimalForAsksPrice
-                    )}
-                  </StyledTypography>
-                </Cell>
-              </Row>
-            ))}
+                    <EmptyCell width={'10%'} />
+                    <Cell width={'45%'}>
+                      <StyledTypography
+                        anime={i === index}
+                        textColor={red[400]}
+                        color="default"
+                        noWrap={true}
+                        variant="body1"
+                        align="right"
+                      >
+                        {Number(order.size).toFixed(
+                          digitsAfterDecimalForAsksSize
+                        )}
+                      </StyledTypography>
+                    </Cell>
+                    <Cell width={'45%'}>
+                      <StyledTypography
+                        anime={i === index}
+                        textColor={red[400]}
+                        color="default"
+                        noWrap={true}
+                        variant="body1"
+                        align="right"
+                      >
+                        {Number(order.price).toFixed(
+                          digitsAfterDecimalForAsksPrice
+                        )}
+                      </StyledTypography>
+                    </Cell>
+                  </RowWithVolumeChart>
+                </Row>
+              )
+            )}
           </>
         )}
       </StyledBody>
@@ -100,17 +110,6 @@ const StyledBody = Body.extend`
       props.tableExpanded
         ? 'calc(60.5vh - 59px - 80px - 39px - 37px - 24px - 26px)'
         : 'calc(99vh - 59px - 80px - 39px - 37px - 24px - 26px)'};
-  }
-`
-
-const StyledTypography = TypographyFullWidth.extend`
-  && {
-    color: ${(props: { textColor: string }) => props.textColor};
-    font-variant-numeric: lining-nums tabular-nums;
-    ${(props: { anime: boolean; textColor: string }) =>
-      props.anime
-        ? `animation: ${opacityAnimation} 300ms cubic-bezier(0.4, 0, 1, 1) 0s 1 normal none running;`
-        : ''};
   }
 `
 
