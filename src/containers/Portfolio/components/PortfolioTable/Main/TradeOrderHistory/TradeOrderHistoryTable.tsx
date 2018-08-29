757e129a3bff5059bc10f62860699bfc20e92ed7
avernikoz <avernikoz@gmail.com>
2018-08-29 14:29:54 +0300

    Refactor:  - moved api queries from TradeOrderHistory to api.ts  - moved styles to TradeOrderHistory.styles.ts

--- src/containers/Portfolio/components/PortfolioTable/Main/TradeOrderHistory/TradeOrderHistoryTable.tsx
+++ src/containers/Portfolio/components/PortfolioTable/Main/TradeOrderHistory/TradeOrderHistoryTable.tsx
@@ -1,41 +1,25 @@
 import * as React from 'react'
-import styled, { css } from 'styled-components'
-import sortIcon from '@icons/arrow.svg'
-import SvgIcon from '@components/SvgIcon/SvgIcon'
+import QueryRenderer from '@components/QueryRenderer'
 
-import TablePlaceholderLoader from '@components/TablePlaceholderLoader'
-import { tradeOrderHistoryTableData } from '@containers/Portfolio/components/PortfolioTable/Main/TradeOrderHistory/mocks'
+import { MyTradesQuery } from '@containers/Portfolio/components/PortfolioTable/Main/TradeOrderHistory/api'
 import {
   formatNumberToUSFormat,
   onSortTableFull,
 } from '@utils/PortfolioTableUtils'
-import { customAquaScrollBar } from '@utils/cssUtils'
+import sortIcon from '@icons/arrow.svg'
+import SvgIcon from '@components/SvgIcon/SvgIcon'
+import TablePlaceholderLoader from '@components/TablePlaceholderLoader'
+// import { tradeOrderHistoryTableData } from '@containers/Portfolio/components/PortfolioTable/Main/TradeOrderHistory/mocks'
 import {
   IProps,
   IState,
   ICurrentSort,
   ITradeOrderHistoryTableData,
 } from '@containers/Portfolio/components/PortfolioTable/Main/TradeOrderHistory/TradeOrderHistoryTable.types'
+import {Wrapper, Table, PT, PTH, PTHC, PTHead, PTR, PTD, PTDC, PTBody, Icon} from './TradeOrderHistroryTable.styles'
 
-import QueryRenderer from '@components/QueryRenderer'
 
-import gql from 'graphql-tag'
 
-export const MyTradesQuery = gql`
-  query MyTrades {
-    myTrades {
-      exchangeId
-      exchange {
-        name
-      }
-      amount
-      cost
-      datetime
-      symbol
-      side
-    }
-  }
-`
 const tradeOrderHistoryTableHeadings = [
   { name: 'Exchange', value: 'exchange' },
   { name: 'Amount', value: 'amount' },
@@ -203,136 +187,3 @@ export default function(props: any) {
     />
   )
 }
-
-const Wrapper = styled.div`
-  ${customAquaScrollBar};
-`
-const Table = styled.table`
-  width: 100%;
-  table-layout: fixed;
-  border-collapse: collapse;
-  display: inline-block;
-`
-
-const PT = css`
-  display: table;
-  width: 100%;
-  position: sticky;
-
-  &::after {
-    content: ' ';
-    position: absolute;
-    left: 0;
-    right: 0;
-    border-bottom: 1px solid white;
-  }
-`
-
-const PTH = css`
-  font-family: Roboto;
-  font-size: 12px;
-  line-height: 24px;
-  color: #fff;
-  text-align: left;
-  font-weight: 500;
-  position: relative;
-  padding: 10px 16px 10px 10px;
-  user-select: none;
-`
-
-const PTHC = styled.th`
-  ${PTH};
-  min-width: 100px;
-  padding-right: 0;
-
-  &:nth-child(1) {
-    min-width: 80px;
-  }
-
-  &:nth-child(2) {
-    min-width: 70px;
-  }
-
-  &:nth-child(4) {
-    min-width: 80px;
-  }
-
-  &:nth-child(5) {
-    min-width: 50px;
-  }
-  
-  &:nth-child(6) {
-    min-width: 150px;
-  }
-`
-
-const PTHead = styled.thead`
-  ${PT};
-  top: 0;
-`
-
-const PTR = styled.tr`
-  cursor: pointer;
-  background-color: ${(props: { isSelected?: boolean }) =>
-    props.isSelected ? '#2d3136' : '#393e44'};
-
-  &:nth-child(even) {
-    background-color: ${(props: { isSelected?: boolean }) =>
-      props.isSelected ? '#2d3a3a' : '#3a4e4e'};
-  }
-`
-
-const PTD = css`
-  color: ${(props: { isSelected?: boolean }) =>
-    props.isSelected ? '#4ed8da' : '#fff'};
-
-  font-family: Roboto;
-  font-size: 12px;
-  line-height: 24px;
-  padding: 1.75px 16px 1.75px 10px;
-  overflow: hidden;
-  white-space: nowrap;
-
-  & svg {
-    width: 15px;
-    height: 15px;
-  }
-`
-const PTDC = styled.td`
-  ${PTD};
-  min-width: 100px;
-  padding-right: 0;
-
-  &:nth-child(1) {
-    min-width: 80px;
-    text-overflow: ellipsis;
-  }
-
-  &:nth-child(2) {
-    min-width: 70px;
-    text-overflow: ellipsis;
-  }
-
-  &:nth-child(4) {
-    min-width: 80px;
-  }
-
-  &:nth-child(5) {
-    min-width: 50px;
-  }
-  
-  &:nth-child(6) {
-    min-width: 150px;
-    text-overflow: ellipsis;
-  }
-`
-
-const PTBody = styled.tbody`
-  display: table;
-  width: 100%;
-  border-bottom: 1px solid #fff;
-`
-
-const Icon = styled.i`
-  padding-right: 5px;
-`
