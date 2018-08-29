0d1f1cbe4176c32b41b1ce66756339aa83d0914e
avernikoz <avernikoz@gmail.com>
2018-08-29 21:19:44 +0300

    moved sc to cssUtils & removed 2 classes from components/SelectExchange & SelectWallet components

--- src/components/SelectExchangeList/SelectExchangeList.tsx
+++ src/components/SelectExchangeList/SelectExchangeList.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react'
 import { getExchangesForKeysListQuery } from '@containers/User/api'
 import QueryRenderer from '@components/QueryRenderer'
-import ReactSelectComponent from '@components/ReactSelectComponent'
-import styled from 'styled-components'
+import { SelectR } from '@utils/cssUtils'
 
-class SelectExchangeList extends React.Component {
-  render() {
-    const { data, ...otherPropsForSelect } = this.props
-    console.log(otherPropsForSelect);
+const SelectExchangeList = ({ data, ...otherPropsForSelect }) => {
+  console.log(otherPropsForSelect)
 
-
-    const exchangeOptions = data.exchangePagination && data.exchangePagination.items
+  const exchangeOptions =
+    data.exchangePagination &&
+    data.exchangePagination.items
       .slice()
       .sort((a, b) => a.name.localeCompare(b.name))
       .map(({ name }) => ({
@@ -18,31 +16,23 @@ class SelectExchangeList extends React.Component {
         value: name,
       }))
 
-
-    return <SelectR placeholder="" options={exchangeOptions || []} {...otherPropsForSelect} />
-  }
+  return (
+    <SelectR
+      placeholder=""
+      options={exchangeOptions || []}
+      {...otherPropsForSelect}
+    />
+  )
 }
 
-
-export default class DataWrapper extends React.Component {
-  render() {
-    return (
-      <QueryRenderer
-        component={SelectExchangeList}
-        query={getExchangesForKeysListQuery}
-        {...this.props}
-      />
-    )
-  }
+const DataWrapper = ({ ...props }) => {
+  return (
+    <QueryRenderer
+      component={SelectExchangeList}
+      query={getExchangesForKeysListQuery}
+      {...props}
+    />
+  )
 }
 
-const SelectR = styled(ReactSelectComponent)`
-  font-family: Roboto;
-  font-size: 16px;
-  border-bottom: 1px solid #c1c1c1;
-  transition: all 200ms cubic-bezier(0.4, 0, 0.2, 1);
-
-  &:hover {
-    border-bottom: 2px solid #fff;
-  }
-`
+export default DataWrapper
--- src/components/SelectWalletList/SelectWalletList.tsx
+++ src/components/SelectWalletList/SelectWalletList.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react'
 import { searchSupportedNetworksQuery } from '@containers/User/api'
 import QueryRenderer from '../QueryRenderer/index'
-import ReactSelectComponent from '../ReactSelectComponent/index'
-import styled from 'styled-components'
+import { SelectR } from '@utils/cssUtils'
 
-class SelectWalletList extends React.Component {
-  render() {
-    const { data, ...otherPropsForSelect } = this.props
-    console.log(this.props, 'this props in wallet');
+const SelectWalletList = ({ data, ...otherPropsForSelect }) => {
+  console.log(otherPropsForSelect, 'this props in wallet')
 
-
-    const walletOptions = data.searchSupportedNetworks && data.searchSupportedNetworks
+  const walletOptions =
+    data.searchSupportedNetworks &&
+    data.searchSupportedNetworks
       .slice()
       .sort((a, b) => a.name.localeCompare(b.name))
       .map(({ name }) => ({
@@ -18,30 +16,23 @@ class SelectWalletList extends React.Component {
         value: name,
       }))
 
-    return <SelectR placeholder="" options={walletOptions || []} {...otherPropsForSelect} />
-  }
+  return (
+    <SelectR
+      placeholder=""
+      options={walletOptions || []}
+      {...otherPropsForSelect}
+    />
+  )
 }
 
-
-export default class DataWrapper extends React.Component {
-  render() {
-    return (
-      <QueryRenderer
-        component={SelectWalletList}
-        query={searchSupportedNetworksQuery}
-        {...this.props}
-      />
-    )
-  }
+const DataWrapper = ({ ...props }) => {
+  return (
+    <QueryRenderer
+      component={SelectWalletList}
+      query={searchSupportedNetworksQuery}
+      {...props}
+    />
+  )
 }
 
-const SelectR = styled(ReactSelectComponent)`
-  font-family: Roboto;
-  font-size: 16px;
-  border-bottom: 1px solid #c1c1c1;
-  transition: all 200ms cubic-bezier(0.4, 0, 0.2, 1);
-
-  &:hover {
-    border-bottom: 2px solid #fff;
-  }
-`
+export default DataWrapper
