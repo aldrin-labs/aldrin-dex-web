785f42ca8fb292d291992d06ecce5555372ed3ef
addamove <empire.empo@gmail.com>
2018-09-07 16:59:14 +0300

    removed redux-obs

--- src/containers/Screener/epics.ts
+++
@@ -1,3 +0,0 @@
-import { ActionsObservable, combineEpics, Epic } from 'redux-observable'
-
-import { test, testNext } from '@containers/Screener/actions'
--- src/utils/rootEpic.ts
+++
@@ -1,6 +0,0 @@
-import { combineEpics, Epic } from 'redux-observable'
-
-import * as screenerEpics from '@containers/Screener/epics'
-import * as userEpics from '@containers/User/epics'
-import * as loginEpics from '@containers/Login/epics'
-import * as portfolioEpics from '@containers/Portfolio/epics'
