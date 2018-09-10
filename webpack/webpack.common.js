ad9429cc472dac36bcd765644635053c1c8e02b4
addamove <empire.empo@gmail.com>
2018-09-10 20:05:02 +0300

    added caching for faster builds on localhost

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,7 @@ const commonPaths = require('./common-paths')
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 
 const config = {
   entry: {
@@ -54,6 +55,38 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'public/index.html',
       // favicon: `public/favicon.ico`
+    }),
+    new HardSourceWebpackPlugin({
+      // Either an absolute path or relative to webpack's options.context.
+      cacheDirectory: 'node_modules/.cache/hard-source/[confighash]',
+      // Either a string of object hash function given a webpack config.
+      configHash: function(webpackConfig) {
+        // node-object-hash on npm can be used to build this.
+        return require('node-object-hash')({ sort: false }).hash(webpackConfig)
+      },
+      // Either false, a string, an object, or a project hashing function.
+      environmentHash: {
+        root: process.cwd(),
+        directories: [],
+        files: ['package-lock.json', 'yarn.lock'],
+      },
+      // An object.
+      info: {
+        // 'none' or 'test'.
+        mode: 'none',
+        // 'debug', 'log', 'info', 'warn', or 'error'.
+        level: 'debug',
+      },
+      // Clean up large, old caches automatically.
+      cachePrune: {
+        // Caches younger than `maxAge` are not considered for deletion. They must
+        // be at least this (default: 2 days) old in milliseconds.
+        maxAge: 2 * 24 * 60 * 60 * 1000,
+        // All caches together must be larger than `sizeThreshold` before any
+        // caches will be deleted. Together they must be at least this
+        // (default: 50 MB) big in bytes.
+        sizeThreshold: 50 * 1024 * 1024,
+      },
     }),
     // new webpack.optimize.CommonsChunkPlugin({
     //   name: ['vendor'],
