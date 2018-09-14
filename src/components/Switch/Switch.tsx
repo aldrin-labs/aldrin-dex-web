a562c68e8a2e78b76a607a596647673ef8678d3b
addamove <empire.empo@gmail.com>
2018-09-14 16:06:51 +0300

    broken test comented

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { Typography } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
 
 interface Props {
   onClick?: Function
@@ -21,7 +21,12 @@ export default class Switch extends React.Component<Props> {
           </Desc>
         )}
         <Label>
-          <Input type="checkbox" onClick={onClick} checked={isActive} />
+          <Input
+            type="checkbox"
+            onChange={() => {}}
+            onClick={onClick}
+            checked={isActive}
+          />
           <Slider />
         </Label>
         {second && (
