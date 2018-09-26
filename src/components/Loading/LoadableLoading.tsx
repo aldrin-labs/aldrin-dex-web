c24ab36a5350baa420a0fe318e60418f33ea6586
avernikoz <avernikoz@gmail.com>
2018-09-26 18:07:10 +0300

    Provide more details when error

--- src/components/Loading/LoadableLoading.tsx
+++ src/components/Loading/LoadableLoading.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import { Loading } from '@components/Loading/Loading'
+import { CustomError } from '@components/ErrorFallback/ErrorFallback'
 
 export default class LoadableLoading extends React.Component {
   render() {
     const { error, timedOut, pastDelay } = this.props
+
     if (error) {
       // When the loader has errored
-      return <div>Error!</div>
+      console.log('ERROR in LoadableLoading: ', error)
+      console.log('error.stack: ', error.stack);
+
+      return <CustomError error={error.message} />
     } else if (timedOut) {
       // When the loader has taken longer than the timeout
       return <div>Taking a long time...</div>
