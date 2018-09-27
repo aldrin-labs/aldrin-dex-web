6f30052fe2aed69cb226539b7bc22405b455602e
avernikoz <avernikoz@gmail.com>
2018-09-27 20:33:07 +0300

    Import styles moved to Import.styles.ts

--- src/containers/Portfolio/components/PortfolioTable/Optimization/Import/Import.tsx
+++ src/containers/Portfolio/components/PortfolioTable/Optimization/Import/Import.tsx
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react'
-import styled from 'styled-components'
 import { ApolloConsumer } from 'react-apollo'
 import MdReplay from '@material-ui/icons/Replay'
 import { Button as ButtonMUI, Typography } from '@material-ui/core'
@@ -15,7 +14,10 @@ import {
 import { OPTIMIZE_PORTFOLIO } from '@containers/Portfolio/components/PortfolioTable/Optimization/api'
 import SelectDates from '@components/SelectTimeRangeDropdown'
 
-class Import extends PureComponent<IProps> {
+import { SwitchButtonsWrapper, HelperForCentering, InputContainer, TableContainer, Input } from './Import.styles'
+
+
+export default class Import extends PureComponent<IProps> {
   componentDidMount() {
     this.importPortfolio()
   }
@@ -293,61 +295,3 @@ class Import extends PureComponent<IProps> {
   }
 }
 
-const SwitchButtonsWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-
-const HelperForCentering = styled.div`
-  width: 224px;
-  min-width: 100px;
-  opacity: 0;
-`
-
-const InputContainer = styled.div`
-  margin: auto 2rem auto 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-width: 100px;
-
-  @media (max-width: 1080px) {
-    margin: auto;
-    flex-wrap: wrap;
-  }
-`
-
-const TableContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  place-content: flex-end;
-  width: 50%;
-  max-width: 50rem;
-  @media (max-width: 600px) {
-    margin-top: 1rem;
-  }
-`
-
-const Input = styled.input`
-  box-sizing: border-box;
-  background: transparent;
-  border-top: none;
-  border-left: none;
-  border-bottom: 2px solid rgba(78, 216, 218, 0.3);
-  outline: none;
-  border-right: none;
-  width: 100%;
-  font-family: Roboto, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  text-align: left;
-  padding: 10px 0 0px;
-  color: rgb(255, 255, 255);
-  transition: all 0.25s ease-out;
-
-  &:focus {
-    border-bottom: 2px solid rgb(78, 216, 218);
-  }
-`
-
-export default Import
