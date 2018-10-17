0c491f83021dd045464ec06335a891fe503af08b
addamove <empire.empo@gmail.com>
2018-10-17 16:11:28 +0300

    changed error message for master

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
 import { ApolloError } from 'apollo-client'
+import { MASTER_BUILD } from '@utils/config'
 
 const Error = styled(Paper)`
   display: flex;
@@ -50,11 +51,19 @@ const RefetchError = (props: { error?: ApolloError; refetch: Function }) => (
   </Fragment>
 )
 
+const ErrorWithoutMessage = () => (
+  <Typography variant="body1" align="center" color="error">
+    {
+      'Oops looks like we broke it somehow! 🙈 We are working on this. Please refresh the page or contact the support.'
+    }
+  </Typography>
+)
+
 export const ErrorFallback = (props: {
   error?: ApolloError
   refetch?: Function
 }) => (
   <Error style={{ margin: 'auto' }} elevation={10}>
-    <SimpleError {...props} />
+    {MASTER_BUILD ? <ErrorWithoutMessage /> : <SimpleError {...props} />}
   </Error>
 )
