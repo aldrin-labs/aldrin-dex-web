4e4f9dbbe8075ee156d735423d233d9393ab65fe
addamove <empire.empo@gmail.com>
2018-10-23 19:06:39 +0300

    refetch queries after deleting

--- src/containers/User/components/CryptoWalletsList/DeleteCryptoWalletDialog.tsx
+++ src/containers/User/components/CryptoWalletsList/DeleteCryptoWalletDialog.tsx
@@ -30,45 +30,45 @@ const DeleteCryptoWalletDialogComponent = ({
   handleSubmit,
   errors,
 }) => (
-    <div>
-      <Button onClick={handleClickOpen}>Delete</Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">
-          Delete crypto wallet {wallet.name}?
+  <div>
+    <Button onClick={handleClickOpen}>Delete</Button>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle id="form-dialog-title">
+        Delete crypto wallet {wallet.name}?
       </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To delete crypto wallet please enter it's name:
+      <DialogContent>
+        <DialogContentText>
+          To delete crypto wallet please enter it's name:
         </DialogContentText>
-          <TextField
-            autoFocus={true}
-            margin="dense"
-            id="cryptoWalletNameInput"
-            name="cryptoWalletNameInput"
-            label="CryptoWallet name"
-            onChange={handleChange}
-            value={values.cryptoWalletNameInput}
-            error={errors && !!errors.cryptoWalletNameInput}
-            type="text"
-            fullWidth={true}
-          />
-          <Typography color="error">{errors.cryptoWalletNameInput}</Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
+        <TextField
+          autoFocus={true}
+          margin="dense"
+          id="cryptoWalletNameInput"
+          name="cryptoWalletNameInput"
+          label="CryptoWallet name"
+          onChange={handleChange}
+          value={values.cryptoWalletNameInput}
+          error={errors && !!errors.cryptoWalletNameInput}
+          type="text"
+          fullWidth={true}
+        />
+        <Typography color="error">{errors.cryptoWalletNameInput}</Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
         </Button>
-          <Button onClick={handleSubmit} color="primary">
-            Delete
+        <Button onClick={handleSubmit} color="primary">
+          Delete
         </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  )
+      </DialogActions>
+    </Dialog>
+  </div>
+)
 
 const formikDialog = withFormik({
   validationSchema: Yup.object().shape({
@@ -78,7 +78,12 @@ const formikDialog = withFormik({
     cryptoWalletNameInput: '',
   }),
   handleSubmit: async ({ cryptoWalletNameInput }, props: any) => {
-    const { wallet, handleClose, deleteCryptoWallet, forceUpdateUserContainer } = props.props
+    const {
+      wallet,
+      handleClose,
+      deleteCryptoWallet,
+      forceUpdateUserContainer,
+    } = props.props
 
     const variables = {
       name: wallet.name,
@@ -135,7 +140,12 @@ const handleState = withStateHandlers(
 )
 
 export const DeleteCryptoWalletDialog = compose(
-  graphql(deleteCryptoWalletMutation, { name: 'deleteCryptoWallet' }),
+  graphql(deleteCryptoWalletMutation, {
+    name: 'deleteCryptoWallet',
+    options: {
+      refetchQueries: ['getKeys', 'getPortfolio', 'portfolios'],
+    },
+  }),
   graphql(getCryptoWalletsQuery),
   handleState,
   formikDialog
