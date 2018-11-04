4424fb897c0fabce43e59f27f7c7a6bf8751db74
addamove <empire.empo@gmail.com>
2018-11-04 22:50:17 +0300

    added we support only binance popup

--- src/containers/User/reducer.ts
+++ src/containers/User/reducer.ts
@@ -5,12 +5,14 @@ import * as actions from '@containers/User/actions'
 const initialState = {
   check: 'lol',
   isShownMocks: false,
+  showBinanceWarning: true,
 }
 
 export default createReducer(
   {
     [actions.addExchangeKey]: (state, payload) => ({ ...state, check: 5 }),
     [actions.testUser]: (state, payload) => ({ ...state, test: 'eeeee booooiii' }),
+    [actions.updateBinanceWarning]: (state, payload) => ({ ...state, showBinanceWarning: payload }),
     [actions.toggleMocks]: (state, payload) => ({
       ...state,
       isShownMocks: !state.isShownMocks,
