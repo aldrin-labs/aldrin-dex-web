4e63d5af106b9a2a30f0ac6539432fe11299c496
addamove <empire.empo@gmail.com>
2018-11-05 20:02:06 +0300

    qf

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,10 @@
+const apiLocalhostFallback = 'develop.api.cryptocurrencies.ai'
+const apiChartLocalhostFallback = 'develop.chart.cryptocurrencies.ai'
+
 export const API_URL =
-  process.env.API_ENDPOINT || 'develop.api.cryptocurrencies.ai'
+  // from netlify.toml or localhostfallback
+  process.env.API_ENDPOINT || apiLocalhostFallback
 export const CHARTS_API_URL =
-  process.env.CHARTS_API_ENDPOINT || 'develop.chart.cryptocurrencies.ai'
+  process.env.CHARTS_API_ENDPOINT || apiChartLocalhostFallback
 // asume that it is master build and not develop one
 export const MASTER_BUILD = process.env.NODE_BUILD === 'master'
