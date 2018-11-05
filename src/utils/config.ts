34f099d4e81d9a91ba426859550be4c933b82929
addamove <empire.empo@gmail.com>
2018-11-05 18:57:49 +0300

    fix config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,24 @@
-export const API_URL =
-  process.env.API_ENDPOINT || 'develop.api.cryptocurrencies.ai'
-export const CHARTS_API_URL =
-  process.env.CHARTS_API_ENDPOINT || 'chart.cryptocurrencies.ai'
-// asume that it is master build and not develop one
-export const MASTER_BUILD = process.env.NODE_BUILD === 'master'
+// https://www.netlify.com/docs/continuous-deployment/#build-environment-variables
+//  process.env.URL are coming from netlify
+const developURL = 'https://develop.beta.cryptocurrencies.ai'
+
+//  also it asume that it is master build and not develop one
+export const MASTER_BUILD = process.env.URL === developURL
+
+// config URLS here
+const chartURL = 'chart.cryptocurrencies.ai'
+const apiURL = 'api.cryptocurrencies.ai'
+
+const addDevelopToURL = (url: string) => `develop.${url}`
+
+export const API_URL = !MASTER_BUILD ? addDevelopToURL(apiURL) : apiURL
+
+export const CHARTS_API_URL = !MASTER_BUILD
+  ? addDevelopToURL(chartURL)
+  : chartURL
+
+// for testing will be here temporary
+window.env.URL = process.env.URL
+window.env.API_URL = API_URL
+window.env.CHARTS_API_URL = CHARTS_API_URL
+window.env.MASTER_BUILD = MASTER_BUILD
