9e533b5ed6114793231bb6067996dfb640479945
addamov <empire.empo@gmail.com>
2018-11-20 19:28:28 +0300

    removed 3 days

--- src/containers/Portfolio/components/PortfolioTable/Optimization/Import/dataForSelector.ts
+++ src/containers/Portfolio/components/PortfolioTable/Optimization/Import/dataForSelector.ts
@@ -1,8 +1,8 @@
 export const RebalancePeriod = [
-  { label: '3 days', value: '3' },
-  { label: '7 days', value: '7'},
-  { label: '14 days', value: '14'},
-  { label: '30 days', value: '30'},
+  // { label: '3 days', value: '3' },
+  { label: '7 days', value: '7' },
+  { label: '14 days', value: '14' },
+  { label: '30 days', value: '30' },
 ]
 
 export const RiskProfile = [
