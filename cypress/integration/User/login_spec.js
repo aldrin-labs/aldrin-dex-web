fd9d4e22a7961119c2db25abcd1a0fc0d14f007e
anibaka <anibaka9@gmail.com>
2018-12-02 19:26:03 +0300

    Start write autoLogin function

--- cypress/integration/User/login_spec.js
+++ cypress/integration/User/login_spec.js
@@ -12,17 +12,9 @@ describe('Login', () => {
       '.auth0-lock-input-show-password > .auth0-lock-input-block > .auth0-lock-input-wrap > .auth0-lock-input'
     ).type('nge')
     cy.get('.auth0-lock-submit').click()
-/*    if (cy.get('[aria-label="Skip"]')) {
-      cy.get('[aria-label="Skip"]').click()
-    }*/
-
     cy.get('.UserLink').should('exist')
   }),
-  it('Login', () => {
-    window.localStorage.setItem(
-      'persist:login',
-      '{"user":"{\"sub\":\"auth0|5bfd48f0301a9f3aa493b8bf\",\"nickname\":\"nge\",\"name\":\"nge@nge.nge\",\"picture\":\"https://s.gravatar.com/avatar/db05d07d32eb64d32742517f4326f21d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fng.png\",\"updated_at\":\"2018-12-02T14:41:19.213Z\",\"email\":\"nge@nge.nge\",\"email_verified\":false}","loginStatus":"true","_persist":"{\"version\":-1,\"rehydrated\":true}"}'
-      )
+  it('Logout', () => {
     cy.visit('/')
     cy.get('.UserLink').should('exist')
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,37 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const auth0 = require('auth0-js');
+
+Cypress.Commands.add('login', (email, password) => {
+    Cypress.log({
+        name: 'loginBySingleSignOn'
+    });
+
+    const webAuth = new auth0.WebAuth({
+        domain: 'my-super-duper-domain.eu.auth0.com', // Get this from https://manage.auth0.com/#/applications and your application
+        clientID: 'myclientid', // Get this from https://manage.auth0.com/#/applications and your application
+        responseType: 'token id_token'
+    });
+
+    webAuth.client.login(
+        {
+            realm: 'Username-Password-Authentication',
+            username: email,
+            password: password,
+            audience: 'localhost:5080', // Get this from https://manage.auth0.com/#/apis and your api, use the identifier property
+            scope: 'openid'
+        },
+        function(err, authResult) {
+            // Auth tokens in the result or an error
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                window.localStorage.setItem('token', authResult.idToken);
+                
+            } else {
+                console.error('Problem logging into Auth0', err);
+                throw err;
+            }
+        }
+    );
+});
