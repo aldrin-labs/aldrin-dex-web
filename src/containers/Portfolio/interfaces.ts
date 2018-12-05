b67b916fdc7619de71c7834ea4b10ca73efb74fb
addamov <empire.empo@gmail.com>
2018-12-05 11:39:16 +0300

    fixed usdt/btc toggle

--- src/containers/Portfolio/interfaces.ts
+++ src/containers/Portfolio/interfaces.ts
@@ -1,9 +1,10 @@
-import { getPortfolioQuery } from '@containers/Portfolio/annotations'
 import React from 'react'
+import { getPortfolioQuery } from '@containers/Portfolio/annotations'
+import { Theme } from '@material-ui/core'
 
 export interface IProps {
   data: { getProfile: getPortfolioQuery; loading: boolean; error?: string }
-  theme: object
+  theme: Theme
   keys: string[]
   activeKeys: string[]
   wallets: string[]
@@ -90,46 +91,12 @@ export interface IndProps {
 }
 
 export interface ITableProps {
-  isShownChart: boolean
+  isUSDCurrently: boolean
+  key: number
+  dustFilter: number
   showTable?: boolean
-  loading: boolean
-  activeKeys: string[]
-  subscription: any
+  baseCoin: 'USDT' | 'BTC'
   onToggleUSDBTC: () => void
-  theme: object
-  toggleWallets: React.ReactEventHandler
-
-  data: {
-    portfolioId: string | null
-    portfolio: {
-      name: string | null
-      processing: boolean | null
-      assetIds: (string | null)[] | null
-      assets:
-        | ({
-            _id: string
-            assetId: string | null
-            exchangeId: string | null
-            keyId: string | null
-            quantity: number | null
-            realizedProfit: number | null
-            unrealizedProfit: number | null
-            totalProfit: number | null
-            asset: {
-              name: string | null
-              symbol: string | null
-              priceUSD: string | null
-              priceBTC: string | null
-            } | null
-            exchange: {
-              name: string | null
-            } | null
-            key: {
-              name: string | null
-              apiKey: string | null
-            } | null
-          } | null)[]
-        | null
-    } | null
-  } | null
+  theme: Theme
+  toggleWallets: () => void
 }
