d5dd73175dc825481d4b80cd081d68e58aaf9689
addamov <empire.empo@gmail.com>
2018-12-14 14:11:17 +0300

    typeRror fixes

--- src/containers/Portfolio/components/PortfolioTable/Industry/DonutChart/DonutChart.tsx
+++ src/containers/Portfolio/components/PortfolioTable/Industry/DonutChart/DonutChart.tsx
@@ -3,16 +3,18 @@ import { DonutChart as Chart } from '@storybook-components/index'
 import { Query } from 'react-apollo'
 
 import { GET_INDUSTRIES } from '../../../../../../queries/portfolio/GET_INDUSTRIES'
-import { withTheme } from '@material-ui/styles'
-import { Theme } from '@material-ui/core'
+import { withTheme, WithTheme } from '@material-ui/styles'
+import { Theme, ConsistentWith } from '@material-ui/core'
 
-const DonutChart = ({ theme }: { theme: Theme }) => {
+const DonutChart: React.FunctionComponent<
+  ConsistentWith<{ theme: Theme }, WithTheme<{}>>
+> = ({ theme }) => {
   return (
     <Query query={GET_INDUSTRIES}>
       {({ data: { portfolioIndustries } }) => {
         return (
           <Chart
-            theme={theme}
+            theme={theme as Theme}
             labelPlaceholder="Industry %"
             data={portfolioIndustries.industries}
             colorLegend={true}
