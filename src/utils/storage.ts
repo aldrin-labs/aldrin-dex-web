323d01cccf6a467e1a47a656ee9d93a240b5400e
avernikoz <avernikoz@gmail.com>
2019-01-07 23:05:44 +0300

    added asyncLocalStorage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,1 +1,14 @@
-export default window.localStorage;
+const asyncLocalStorage = {
+  setItem: (key, value) => {
+    return Promise.resolve().then(() => {
+      window.localStorage.setItem(key, value)
+    })
+  },
+  getItem: (key) => {
+    return Promise.resolve().then(() => {
+      return window.localStorage.getItem(key)
+    })
+  },
+}
+
+export default asyncLocalStorage
