477af7709aabe4b8e44ff76172294207ee86f347
avernikoz <avernikoz@gmail.com>
2019-01-09 00:45:37 +0300

    added handling for loading/error in withAuth hoc

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -7,6 +7,9 @@ import { graphql, Mutation, Query } from 'react-apollo'
 import { UPDATE_LOGIN_POPUP_APPEARED } from '@core/graphql/mutations/ui/updateLoginPopupAppeared'
 import { LOGIN_POPUP_APPEARED } from '@core/graphql/queries/ui/LOGIN_POPUP_APPEARED'
 import { GET_LOGIN_DATA } from '@core/graphql/queries/login/GET_LOGIN_DATA'
+// TODO: add hooks or better handling for queries
+
+
 
 const Result = (Component: React.ComponentType) => ({
   // login,
@@ -19,20 +22,34 @@ const Result = (Component: React.ComponentType) => ({
   return (
     <Query query={GET_LOGIN_DATA}>
       {({
-        data: {
-          login: { loginStatus, modalIsOpen },
-        },
-      }) => {
+        // data: {
+        //   login: { loginStatus, modalIsOpen },
+        // },
+          loading, data, error,
+        }) => {
+        if (loading) return 'Loading...'
+        if (error) return `Error!`
+
+        const loginStatus = data.login.loginStatus
+        const modalIsOpen = data.login.modalIsOpen
+
+
         if (!loginStatus) {
           return (
             <Mutation mutation={UPDATE_LOGIN_POPUP_APPEARED}>
               {(updateLoginPopupAppeared) => (
                 <Query query={LOGIN_POPUP_APPEARED}>
                   {({
-                    data: {
-                      ui: { logInPopupAppeared },
-                    },
+                    // data: {
+                    //   ui: { logInPopupAppeared },
+                    // },
+                    loading, data, error,
                   }) => {
+                    if (loading) return 'Loading...'
+                    if (error) return `Error!`
+
+                    const logInPopupAppeared = data.ui.logInPopupAppeared
+
                     const render = (
                       <YouNeedToLoginMessage
                         open={!modalIsOpen}
