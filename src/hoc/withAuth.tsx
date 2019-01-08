39d668d9eea899d00368678b0d778badd54f98a6
avernikoz <avernikoz@gmail.com>
2019-01-08 22:36:17 +0300

    refactored withAuth to support apollo

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -3,56 +3,66 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 
 import YouNeedToLoginMessage from '@components/YouNotLoginedCard'
-import { Mutation, Query } from 'react-apollo'
+import { graphql, Mutation, Query } from 'react-apollo'
 import { UPDATE_LOGIN_POPUP_APPEARED } from '@core/graphql/mutations/ui/updateLoginPopupAppeared'
 import { LOGIN_POPUP_APPEARED } from '@core/graphql/queries/ui/LOGIN_POPUP_APPEARED'
+import { GET_LOGIN_DATA } from '@core/graphql/queries/login/GET_LOGIN_DATA'
 
 const Result = (Component: React.ComponentType) => ({
-  login,
-  openMessage,
+  // login,
+  // openMessage,
   ...props
 }: {
-    login: boolean
-    openMessage: boolean
-  }) => {
-  if (!login) {
-    return (
-      <Mutation mutation={UPDATE_LOGIN_POPUP_APPEARED}>
-        {(updateLoginPopupAppeared) => (
-          <Query query={LOGIN_POPUP_APPEARED}>
-            {({
-              data: {
-                ui: { logInPopupAppeared },
-              },
-            }) => {
-              const render = (
-                <YouNeedToLoginMessage
-                  open={!openMessage}
-                  showModalAfterDelay={logInPopupAppeared ? false : 1500}
-                />
-              )
-              // show login auth0 popup only once
-              if (!logInPopupAppeared) {
-                updateLoginPopupAppeared()
-              }
+  // login: boolean
+  // openMessage: boolean
+}) => {
+  return (
+    <Query query={GET_LOGIN_DATA}>
+      {({
+        data: {
+          login: { loginStatus, modalIsOpen },
+        },
+      }) => {
+        if (!loginStatus) {
+          return (
+            <Mutation mutation={UPDATE_LOGIN_POPUP_APPEARED}>
+              {(updateLoginPopupAppeared) => (
+                <Query query={LOGIN_POPUP_APPEARED}>
+                  {({
+                    data: {
+                      ui: { logInPopupAppeared },
+                    },
+                  }) => {
+                    const render = (
+                      <YouNeedToLoginMessage
+                        open={!modalIsOpen}
+                        showModalAfterDelay={logInPopupAppeared ? false : 1500}
+                      />
+                    )
+                    // show login auth0 popup only once
+                    if (!logInPopupAppeared) {
+                      updateLoginPopupAppeared()
+                    }
 
-              return render
-            }}
-          </Query>
-        )}
-      </Mutation>
-    )
-  }
+                    return render
+                  }}
+                </Query>
+              )}
+            </Mutation>
+          )
+        }
 
-  return <Component {...props} />
+        return <Component {...props} />
+      }}
+    </Query>
+  )
 }
 
-const mapStateToProps = (store: any) => ({
-  login: store.login.loginStatus,
-  openMessage: store.login.modalIsOpen,
-})
+// const mapStateToProps = (store: any) => ({
+//   login: store.login.loginStatus,
+//   openMessage: store.login.modalIsOpen,
+// })
 
-export default compose(
-  connect(mapStateToProps),
-  Result
-)
+export default compose()(Result)
+// connect(mapStateToProps),
+// graphql(GET_LOGIN_DATA, { name: 'loginDataQuery' }),
