267fd30067f9522b40ff05f3dce30f0cb835424a
avernikoz <avernikoz@gmail.com>
2019-01-10 13:41:34 +0300

    update async localstorage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,12 +1,27 @@
 const asyncLocalStorage = {
-  setItem: (key, value) => {
+  setItem: (key: string, value) => {
     return Promise.resolve().then(() => {
       window.localStorage.setItem(key, value)
     })
   },
-  getItem: (key) => {
+  getItem: (key: string) => {
     return Promise.resolve().then(() => {
       return window.localStorage.getItem(key)
+    })
+  },
+  removeItem: (key: string) => {
+    return Promise.resolve().then(() => {
+      return window.localStorage.removeItem(key)
+    })
+  },
+  clear: () => {
+    return Promise.resolve().then(() => {
+      return window.localStorage.clear()
+    })
+  },
+  key: (n: number) => {
+    return Promise.resolve().then(() => {
+      return window.localStorage.key(n)
     })
   },
 }
