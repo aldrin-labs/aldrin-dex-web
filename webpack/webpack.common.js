07950a5abc3f932d53c1d345312ee06f4f2c1582
avernikoz <avernikoz@gmail.com>
2019-01-23 16:39:14 +0300

    added routes alias in web

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -26,6 +26,7 @@ const config = {
         'core',
         'src',
       ),
+      '@routes': path.join(__dirname, '..', 'src', 'routes'),
       '@components': path.join(__dirname, '..', 'src', 'components'),
       '@containers': path.join(__dirname, '..', 'src', 'containers'),
       '@utils': path.join(__dirname, '..', 'src', 'utils'),
