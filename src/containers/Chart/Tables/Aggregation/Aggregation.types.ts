ad2415b3adb05cd66007582fd7f7e55ea0fd28fd
avernikoz <avernikoz@gmail.com>
2019-01-24 01:20:02 +0300

    delete chart

--- src/containers/Chart/Tables/Aggregation/Aggregation.types.ts
+++
@@ -1,5 +0,0 @@
-export interface IProps {
-  aggregation: number
-  onButtonClick: (event: any) => any
-  theme: any //specify
-}
--- src/containers/Chart/Tables/Tables.tsx
+++
@@ -1,6 +0,0 @@
-import OrderBookTable from './OrderBookTable/OrderBookTableContainer'
-import Aggregation from './Aggregation/Aggregation'
-import TradeHistoryTable from './TradeHistoryTable/TableContainer'
-import ExchangesTable from './ExchangesTable/TableContainer'
-
-export { Aggregation, OrderBookTable, ExchangesTable, TradeHistoryTable }
