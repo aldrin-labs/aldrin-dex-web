f7dcf0036173ca9b67382083234756ac46f55a0a
avernikoz <avernikoz@gmail.com>
2019-05-07 16:50:32 +0300

    add dev:fast:local in package.json for run local backend development & add LOCAL_BUILD variable in webpack.dev.js

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -67,6 +67,7 @@ const config = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
+        LOCAL_BUILD: JSON.stringify(process.env.LOCAL_BUILD),
         NODE_ENV: JSON.stringify('development'),
         API_ENDPOINT: JSON.stringify(process.env.API_ENDPOINT),
         CHARTS_API_ENDPOINT: JSON.stringify(process.env.CHARTS_API_ENDPOINT),
