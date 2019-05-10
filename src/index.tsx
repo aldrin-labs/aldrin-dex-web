1974046e31c3767f5aea90a39c05d1a2bd19a2ef
anibaka <anibaka9@gmail.com>
2019-05-10 17:42:42 +0300

    moove back with routes

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,10 @@ import ReactDOM from 'react-dom'
 import { hot } from 'react-hot-loader'
 import { IntlProvider } from 'react-intl'
 import { Provider } from 'react-redux'
-import { Route, Switch, Redirect } from 'react-router'
-import { ConnectedRouter } from 'react-router-redux'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 
 import { App } from '@sb/compositions/App/'
-import { MainApp } from '@sb/compositions/App/MainApp'
 import { client } from '@core/graphql/apolloClient'
 import { persistor, store } from '@core/redux/configureStore'
 import { ErrorBoundary } from '@sb/components/index'
@@ -35,37 +33,28 @@ const render = () =>
       <IntlProvider locale="en">
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
-            <ConnectedRouter history={history}>
+            <BrowserRouter>
               <App>
                 <ErrorBoundary>
                   <Suspense fallback={<Loading centerAligned />}>
-                  <Switch>
-                    <Route path="/registration" component={OnboardingRoutes} />
-                    <Route path="/">
-                    <MainApp>
-                      <Switch>
-                        <Redirect from="/" to="/portfolio" exact />
-                        <Redirect from="/portfolio" to="/portfolio/main" exact />
+                    <Switch>
+                      <Route path="/registration" component={OnboardingRoutes} />
+                      <Redirect from="/" to="/portfolio/main" exact />
+                      <Redirect from="/portfolio" to="/portfolio/main" exact />
 
-                        {/*<Route exact path="/" component={HomeRoutes} />*/}
-                        {/*<Route exact path="/profile" component={ProfileRoutes} />*/}
-                        <Route
-                          path="/portfolio"
-                          component={PortfolioRoutes}
-                        />
-                        {<Route exact path="/market" component={MarketRoutes} />}
-                        <Route exact path="/chart" component={ChartRoutes} />
-                        {/*<Route exact path="/screener" component={ScreenerRoutes} />x*/}
-                        <Route exact path="/user" component={UserRoutes} />
-                        <Route path="*" component={NotFound} />
-                      </Switch>
-                      </MainApp>
-                      </Route>
+                      {/*<Route exact path="/" component={HomeRoutes} />*/}
+                      {/*<Route exact path="/profile" component={ProfileRoutes} />*/}
+                      <Route path="/portfolio" component={PortfolioRoutes} />
+                      {<Route exact path="/market" component={MarketRoutes} />}
+                      <Route exact path="/chart" component={ChartRoutes} />
+                      {/*<Route exact path="/screener" component={ScreenerRoutes} />x*/}
+                      <Route exact path="/user" component={UserRoutes} />
+                      <Route path="*" component={NotFound} />
                     </Switch>
                   </Suspense>
                 </ErrorBoundary>
               </App>
-            </ConnectedRouter>
+            </BrowserRouter>
           </PersistGate>
         </Provider>
       </IntlProvider>
