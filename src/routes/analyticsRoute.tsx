3d09d31cc6dc72ea789f20c0c48f9cc7926d0b0a
Likbes <likbes.js@gmail.com>
2021-02-17 20:42:25 +0200

    added fonts and packages for analytics page

--- src/routes/analyticsRoute.tsx
+++ src/routes/analyticsRoute.tsx
@@ -3,9 +3,15 @@ import { Route, Switch } from 'react-router-dom'
 import AnalyticsRoute from '@sb/compositions/AnalyticsRoute/index'
 
 export default function AnalyticsRoutes({ match }) {
+  console.log('match', match)
   return (
     <Switch>
-      <Route path={match.url} component={AnalyticsRoute} />
+      <Route
+        path="/analytics/:selectedPair"
+        render={({ match: { params } }) => {
+          return <AnalyticsRoute selectedPair={params.selectedPair} />
+        }}
+      />
     </Switch>
   )
 }
