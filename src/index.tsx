8f7bbe762cbd9f144330d95857d1a6337e71ba6b
avernikoz <avernikoz@gmail.com>
2021-08-17 11:58:44 +0300

    redirect from CCAI_USDC to RIN_USDC

--- src/index.tsx
+++ src/index.tsx
@@ -102,6 +102,7 @@ const render = () =>
                 <Redirect from="/" to={'/chart'} exact />
                 <Redirect from="/chart" to="/chart/spot" exact />
                 <Redirect from="/chart/spot" to="/chart/spot/RIN_USDC" exact />
+                <Redirect from="/chart/spot/CCAI_USDC" to="/chart/spot/RIN_USDC" exact />
                 <Redirect from="/chart/futures" to="/chart/spot/RIN_USDC" />
                 <Redirect from="/analytics" to="/analytics/all" exact />
                 <Redirect from="/rewards" to="/" exact />
