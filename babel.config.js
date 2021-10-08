0b3a9615762103b261454172ce3d457e5aa167c1
Dmitry Modin <xzwiex@lembrand.com>
2021-10-08 11:36:30 +0300

    Add eslint

--- babel.config.js
+++ babel.config.js
@@ -5,149 +5,19 @@ module.exports = {
       {
         useBuiltIns: 'usage', // alternative mode: "entry"
         corejs: 3, // default would be 2
-        targets: '> 0.25%, not dead'
+        targets: '> 0.25%, not dead',
       },
     ],
-    '@babel/react',
+    [
+      '@babel/preset-react',
+      {
+        development: process.env.NODE_ENV === 'development',
+      },
+    ],
     '@babel/preset-typescript',
   ],
   plugins: [
+    'react-hot-loader/babel',
     'graphql-tag',
-  ]
-  env: {
-    // test: {
-    //   presets: [
-    //     [
-    //       '@babel/preset-env',
-    //       {
-    //         modules: false,
-    //         targets: {
-    //           browsers: ['last 2 versions'],
-    //         },
-    //       },
-    //     ],
-    //     '@babel/react',
-    //     '@babel/preset-typescript',
-    //   ],
-    //   plugins: [
-    //     'react-hot-loader/babel',
-    //     [
-    //       'babel-plugin-styled-components',
-    //       {
-    //         minify: true,
-    //         transpileTemplateLiterals: true,
-    //         pure: true,
-    //       },
-    //     ],
-    //     'graphql-tag',
-    //     '@babel/plugin-syntax-dynamic-import',
-    //     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    //     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    //     '@babel/plugin-proposal-export-default-from',
-    //     '@babel/plugin-proposal-export-namespace-from',
-    //     '@babel/plugin-proposal-function-sent',
-    //     '@babel/plugin-proposal-json-strings',
-    //     '@babel/plugin-proposal-logical-assignment-operators',
-    //     '@babel/plugin-proposal-nullish-coalescing-operator',
-    //     '@babel/plugin-proposal-numeric-separator',
-    //     '@babel/plugin-proposal-optional-chaining',
-    //     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    //     '@babel/plugin-proposal-throw-expressions',
-    //     '@babel/plugin-syntax-import-meta',
-    //     '@babel/plugin-transform-react-inline-elements',
-    //   ],
-    // },
-    // production: {
-    //   presets: [
-    //     [
-    //       '@babel/preset-env',
-    //       {
-    //         useBuiltIns: 'usage',
-    //         targets: {
-    //           browsers: ['last 2 Chrome versions'],
-    //         },
-    //         modules: false,
-    //       },
-    //     ],
-    //     '@babel/react',
-    //     '@babel/preset-typescript',
-    //   ],
-    //   plugins: [
-    //     'graphql-tag',
-    //     [
-    //       'babel-plugin-styled-components',
-    //       {
-    //         minify: true,
-    //         transpileTemplateLiterals: true,
-    //         pure: true,
-    //       },
-    //     ],
-    //     ['transform-react-remove-prop-types'],
-    //     '@babel/plugin-syntax-dynamic-import',
-    //     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    //     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    //     '@babel/plugin-proposal-export-default-from',
-    //     '@babel/plugin-proposal-export-namespace-from',
-    //     '@babel/plugin-proposal-function-sent',
-    //     '@babel/plugin-proposal-json-strings',
-    //     '@babel/plugin-proposal-logical-assignment-operators',
-    //     '@babel/plugin-proposal-nullish-coalescing-operator',
-    //     '@babel/plugin-proposal-numeric-separator',
-    //     '@babel/plugin-proposal-optional-chaining',
-    //     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    //     '@babel/plugin-proposal-throw-expressions',
-    //     '@babel/plugin-syntax-import-meta',
-    //     '@babel/plugin-transform-react-inline-elements',
-    //   ],
-    // },
-    // development: {
-    //   presets: [
-    //     [
-    //       '@babel/preset-env',
-    //       {
-    //         targets: {
-    //           browsers: ['last 2 versions'],
-    //         },
-    //         modules: false,
-    //       },
-    //     ],
-    //     '@babel/react',
-    //     '@babel/preset-typescript',
-    //   ],
-    //   plugins: [
-    //     // 'react-hot-loader/babel',
-    //     // [
-    //     //   'babel-plugin-styled-components',
-    //     //   {
-    //     //     minify: true,
-    //     //     transpileTemplateLiterals: true,
-    //     //     pure: true,
-    //     //   },
-    //     // ],
-    //     // 'graphql-tag',
-    //     // [
-    //     //   '@babel/plugin-transform-runtime',
-    //     //   {
-    //     //     helpers: false,
-    //     //     regenerator: true,
-    //     //   },
-    //     // ],
-    //     // '@babel/plugin-syntax-dynamic-import',
-    //     // ['@babel/plugin-proposal-decorators', { legacy: true }],
-    //     // ['@babel/plugin-proposal-class-properties', { loose: true }],
-    //     // '@babel/plugin-proposal-export-default-from',
-    //     // '@babel/plugin-proposal-export-namespace-from',
-    //     // '@babel/plugin-proposal-function-sent',
-    //     // '@babel/plugin-proposal-json-strings',
-    //     // '@babel/plugin-proposal-logical-assignment-operators',
-    //     // '@babel/plugin-proposal-nullish-coalescing-operator',
-    //     // '@babel/plugin-proposal-numeric-separator',
-    //     // '@babel/plugin-proposal-optional-chaining',
-    //     // ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    //     // '@babel/plugin-proposal-throw-expressions',
-    //     // '@babel/plugin-syntax-import-meta',
-    //     // '@babel/plugin-transform-react-inline-elements',
-    //   ],
-    // },
-  },
+  ],
 }
