65a5b27674ae2b6b832b8d46fc60cb785d491a0d
Dmitry Modin <xzwiex@lembrand.com>
2021-11-02 16:20:36 +0200

    UP

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -77,6 +77,7 @@ const config = {
     //   },
     // },
   // },
+  stats: 'verbose',
   plugins: [
     new ImageminPlugin({ test: /\.(jpe?g|png)$/i }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
