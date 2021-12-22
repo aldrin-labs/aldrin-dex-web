66bb8a8c38fcf71c5705dde45f7d01a333d47d80
Dmitry Modin <xzwiex@gmail.com>
2021-12-22 07:59:17 +0000

    Permissionless pools

--- babel.config.js
+++ babel.config.js
@@ -1,139 +1,20 @@
 module.exports = {
-  env: {
-    test: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            modules: false,
-            targets: {
-              browsers: ['last 2 versions'],
-            },
-          },
-        ],
-        '@babel/react',
-        '@babel/preset-typescript',
-      ],
-      plugins: [
-        'react-hot-loader/babel',
-        [
-          'babel-plugin-styled-components',
-          {
-            minify: true,
-            transpileTemplateLiterals: true,
-            pure: true,
-          },
-        ],
-        'graphql-tag',
-        '@babel/plugin-syntax-dynamic-import',
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-proposal-export-namespace-from',
-        '@babel/plugin-proposal-function-sent',
-        '@babel/plugin-proposal-json-strings',
-        '@babel/plugin-proposal-logical-assignment-operators',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-numeric-separator',
-        '@babel/plugin-proposal-optional-chaining',
-        ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-        '@babel/plugin-proposal-throw-expressions',
-        '@babel/plugin-syntax-import-meta',
-        '@babel/plugin-transform-react-inline-elements',
-      ],
-    },
-    production: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            useBuiltIns: 'usage',
-            targets: {
-              browsers: ['last 2 Chrome versions'],
-            },
-            modules: false,
-          },
-        ],
-        '@babel/react',
-        '@babel/preset-typescript',
-      ],
-      plugins: [
-        ['lodash', { id: ['lodash', 'recompose'] }],
-        'graphql-tag',
-        [
-          'babel-plugin-styled-components',
-          {
-            minify: true,
-            transpileTemplateLiterals: true,
-            pure: true,
-          },
-        ],
-        ['transform-react-remove-prop-types'],
-        '@babel/plugin-syntax-dynamic-import',
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-proposal-export-namespace-from',
-        '@babel/plugin-proposal-function-sent',
-        '@babel/plugin-proposal-json-strings',
-        '@babel/plugin-proposal-logical-assignment-operators',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-numeric-separator',
-        '@babel/plugin-proposal-optional-chaining',
-        ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-        '@babel/plugin-proposal-throw-expressions',
-        '@babel/plugin-syntax-import-meta',
-        '@babel/plugin-transform-react-inline-elements',
-      ],
-    },
-    development: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              browsers: ['last 2 versions'],
-            },
-            modules: false,
-          },
-        ],
-        '@babel/react',
-        '@babel/preset-typescript',
-      ],
-      plugins: [
-        'react-hot-loader/babel',
-        [
-          'babel-plugin-styled-components',
-          {
-            minify: true,
-            transpileTemplateLiterals: true,
-            pure: true,
-          },
-        ],
-        'graphql-tag',
-        [
-          '@babel/plugin-transform-runtime',
-          {
-            helpers: false,
-            regenerator: true,
-          },
-        ],
-        '@babel/plugin-syntax-dynamic-import',
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-proposal-export-namespace-from',
-        '@babel/plugin-proposal-function-sent',
-        '@babel/plugin-proposal-json-strings',
-        '@babel/plugin-proposal-logical-assignment-operators',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-numeric-separator',
-        '@babel/plugin-proposal-optional-chaining',
-        ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-        '@babel/plugin-proposal-throw-expressions',
-        '@babel/plugin-syntax-import-meta',
-        '@babel/plugin-transform-react-inline-elements',
-      ],
-    },
-  },
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage', // alternative mode: "entry"
+        corejs: 3, // default would be 2
+        targets: '> 0.25%, not dead',
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        development: process.env.NODE_ENV === 'development',
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: ['react-hot-loader/babel', 'graphql-tag', 'babel-plugin-styled-components'],
 }
