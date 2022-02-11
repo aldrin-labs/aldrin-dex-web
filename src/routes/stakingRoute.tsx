d8c0affc0a81557f462acc4b581f3f34475f9f00
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-02-11 11:46:03 +0200

    staking service wip

--- src/routes/stakingRoute.tsx
+++ src/routes/stakingRoute.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import { Staking } from '@sb/compositions/Staking'
+
+import { StakingPage } from '@sb/compositions/Staking'
 
 export default function StakingRoute({ match }) {
   return (
     <Switch>
-      <Route path={match.url} component={Staking} />
+      <Route path={match.url} component={StakingPage} />
     </Switch>
   )
 }
