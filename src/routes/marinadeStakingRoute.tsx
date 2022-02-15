a04aac1c385816572d015b180b29d3191ab9c44d
Dmitry Modin <xzwiex@lembrand.com>
2022-02-15 16:48:09 +0200

    Bump

--- src/routes/marinadeStakingRoute.tsx
+++ src/routes/marinadeStakingRoute.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import { MarinadeStaking } from '@sb/compositions/MarinadeStaking/index'
+import { MarinadeStaking } from '@sb/compositions/MarinadeStaking'
 
 export default function MarinadeStakingRoute({ match }) {
   return (
