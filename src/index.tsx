24f45d2e1ab1c2a475c44ed1287bf7960c32d040
Dmitry Modin <xzwiex@lembrand.com>
2022-02-17 14:50:30 +0200

    Fix review

--- src/index.tsx
+++ src/index.tsx
@@ -56,15 +56,14 @@ const SwapRoutes = lazy(() => import(/* webpackChunkName: "swap" */ '@routes/swa
 const DashboardRoute = lazy(() => import(/* webpackChunkName: "dashboard" */ '@routes/dashboardRoute'))
 
 const StakingRoute = lazy(() => import(/* webpackChunkName: "staking" */ '@routes/stakingRoute'))
-<<<<<<< HEAD
+
 const TwammRoute = lazy(() => import(/* webpackChunkName: "rebalance" */ '@routes/twammRoute'))
 
-const RinStakingRoute = lazy(() => import(/* webpackChunkName: "staking" */ '@routes/rinStakingRoute'))
-
-const MarinadeStakingRoute = lazy(() => import(/* webpackChunkName: "staking" */ '@routes/marinadeStakingRoute'))
-=======
 const RinStakingRoute = lazy(() => import(/* webpackChunkName: "rinStaking" */ '@routes/rinStakingRoute'))
->>>>>>> b2f4557ab (Bump)
+
+const MarinadeStakingRoute = lazy(
+  () => import(/* webpackChunkName: "marinadeStaking" */ '@routes/marinadeStakingRoute')
+)
 
 // const HomepageRoute = lazy(() => import('@routes/homeRoute'))
 
@@ -106,7 +105,6 @@ const AppRoot = () => (
                 <Route path="/chart" component={ChartRoutes} />
                 <Route path="/analytics" component={AnalyticsRoute} />
                 <Route path="/dashboard" component={DashboardRoute} />
-<<<<<<< HEAD
 
                 <Route path="/staking/marinade" component={MarinadeStakingRoute} />
                 <Route path="/staking/rin" component={RinStakingRoute} />
@@ -114,12 +112,6 @@ const AppRoot = () => (
 
                 {!MASTER_BUILD && <Route path="/addressbook" component={AddressbookRoute} />}
                 <Route path="/pools" component={PoolsRoute} />
-=======
-                {!MASTER_BUILD && <Route path="/addressbook" component={AddressbookRoute} />}
-                <Route path="/pools" component={PoolsRoute} />
-                <Route path="/staking/rin" component={RinStakingRoute} />
-                <Route path="/staking" component={StakingRoute} />
->>>>>>> b2f4557ab (Bump)
 
                 <Route path="/rebalance" component={RebalanceRoute} exact />
                 <Route path="/restrictedRegion" component={RestrictedRegionRoute} exact />
