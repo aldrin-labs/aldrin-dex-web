3d75bb85747daca48e292002d5e13167db8fa785
Dmitry Modin <xzwiex@lembrand.com>
2022-03-11 13:25:30 +0200

    UP

--- src/index.tsx
+++ src/index.tsx
@@ -56,11 +56,6 @@ const SwapRoutes = lazy(() => import(/* webpackChunkName: "swap" */ '@routes/swa
 const DashboardRoute = lazy(() => import(/* webpackChunkName: "dashboard" */ '@routes/dashboardRoute'))
 
 const StakingRoute = lazy(() => import(/* webpackChunkName: "staking" */ '@routes/stakingRoute'))
-const RinStakingRoute = lazy(() => import(/* webpackChunkName: "rinStaking" */ '@routes/rinStakingRoute'))
-
-const MarinadeStakingRoute = lazy(
-  () => import(/* webpackChunkName: "marinadeStaking" */ '@routes/marinadeStakingRoute')
-)
 
 const TwammRoute = lazy(() => import(/* webpackChunkName: "rebalance" */ '@routes/twammRoute'))
 
@@ -78,6 +73,7 @@ const isSafari =
 const SWR_CONFIG = {
   revalidateOnFocus: false,
 }
+
 const AppRoot = () => (
   <ApolloProvider client={client}>
     <GlobalStyle />
