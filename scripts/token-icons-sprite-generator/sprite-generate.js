67921415e8c46bb8edc4310b0b04c6b659f32eea
Aleksei D <derushev.aleksei@gmail.com>
2022-06-15 00:27:59 +0300

    temp progress

--- scripts/token-icons-sprite-generator/sprite-generate.js
+++ scripts/token-icons-sprite-generator/sprite-generate.js
@@ -4,11 +4,10 @@ const { createHttpLink } = require('apollo-link-http')
 const { InMemoryCache } = require('apollo-cache-inmemory')
 const fetch = require('node-fetch')
 const { TokenListProvider } = require('@solana/spl-token-registry')
-const http = require('https')
+const http = require('follow-redirects').https
 const fs = require('fs')
 const path = require('path')
 const Spritesmith = require('spritesmith')
-const util = require('util')
 const sharp = require('sharp')
 
 const markets = require('./../../src/core/src/utils/awesomeMarkets/markets.json')
@@ -81,20 +80,7 @@ const run = async () => {
   })
 
   const poolSymbolsList = getPoolsInfo.reduce((curr, next) => {
-    const isTokenAInList = curr.includes(next.tokenA)
-    const isTokenBInList = curr.includes(next.tokenB)
-
-    const toAdd = []
-
-    if (!isTokenAInList) {
-      toAdd.push(next.tokenA)
-    }
-
-    if (!isTokenBInList) {
-      toAdd.push(next.tokenB)
-    }
-
-    return [...curr, ...toAdd]
+    return [...curr, next.tokenA, next.tokenB]
   }, [])
 
   console.log(`[1/10] Getting aldrin tokens from pools. Found: ${poolSymbolsList.length}`)
@@ -134,94 +120,89 @@ const run = async () => {
   }
 
   try {
-    // fs.rmSync(`${__dirname}/icons`, { recursive: true, force: true })
-    // fs.rmSync(`${__dirname}/output`, { recursive: true, force: true })
-    //
-    // fs.mkdirSync(`${__dirname}/output`)
-    // fs.mkdirSync(`${__dirname}/icons`)
+    fs.rmSync(`${__dirname}/icons`, { recursive: true, force: true })
+    fs.rmSync(`${__dirname}/output`, { recursive: true, force: true })
+
+    fs.mkdirSync(`${__dirname}/output`)
+    fs.mkdirSync(`${__dirname}/icons`)
   } catch (e) {
     console.log('Error: ', e)
   }
 
-  // for (let i = 0; i < tokensAldrinList.length; i++) {
-  //   const item = tokensAldrinList[i]
-  //   const ext = path.extname(item.logoUrl)
-  //   const extToStore = ALLOWED_EXTENSION.includes(ext) ? ext : '.png'
-  //
-  //   try {
-  //     process.stdout.write(`[3/10] Downloading: ${item.symbol} (${i+1}/${tokensAldrinList.length})\r`)
-  //     await download(item.logoUrl, `${__dirname}/icons/${item.symbol}-${item.mint}${extToStore}`)
-  //   } catch(e) {
-  //     console.log('Error: ', e)
-  //   }
-  // }
+  for (let i = 0; i < tokensAldrinList.length; i++) {
+    const item = tokensAldrinList[i]
+    const ext = path.extname(item.logoUrl)
+    const extToStore = ALLOWED_EXTENSION.includes(ext) ? ext : '.png'
+
+    try {
+      process.stdout.write(`[5/10] Downloading: ${item.symbol} (${i+1}/${tokensAldrinList.length})\r`)
+      await download(item.logoUrl, `${__dirname}/icons/${item.symbol}-${item.mint}${extToStore}`)
+    } catch(e) {
+      console.log('Error: ', e)
+    }
+  }
 
   const images = fs.readdirSync(`${__dirname}/icons`)
 
-  console.log(`[5/10] Compressing images`)
+  console.log(`[6/10] Compressing images`)
 
-  const resizePromises = images.map((item) => {
-    return sharp(`${__dirname}/icons/${item}`).resize({ width: 64 }).toBuffer((err, buffer) => {
-      return fs.writeFile(`${__dirname}/icons/${item}`, buffer, () => {});
-    });
-  })
+  for (let i = 0; i < images.length; i++) {
+    const item = images[i];
+    const buffer = await sharp(`${__dirname}/icons/${item}`).resize(64).toBuffer();
 
-  await Promise.all(resizePromises)
+    fs.writeFileSync(`${__dirname}/icons/${item}`, buffer, () => {});
+  }
 
-  console.log(`[6/10] Creating sprite`)
+  console.log(`[7/10] Creating sprite`)
 
   Spritesmith.run({
     src: images.map((item) => `${__dirname}/icons/${item}`),
     engine: require('canvassmith'),
-    cssOpts: {
-      cssClass: (item) => {
-        return util.format('.token-icon-%s:before', item.name)
-      },
-    },
   }, async (err, result) => {
     if (err) {
       throw err;
     }
 
-    fs.writeFileSync(`${__dirname}/output/token-icons.png`, result.image);
+    console.log(`[8/10] Converting to webp`)
 
-    console.log(`[7/10] Converting to webp`)
+    await sharp(result.image).webp().toFile(`${__dirname}/output/token-icons.webp`)
 
-    await sharp(`${__dirname}/output/token-icons.png`).webp({ quality: 80 }).toFile(`${__dirname}/output/token-icons.webp`)
+    const styles = Object.keys(result.coordinates).reduce((curr, next) => {
+      const coords = result.coordinates[next]
+      const parsedPath = path.parse(next)
 
-    console.log(`[8/10] Creating css`)
-
-    const styles = Object.keys(result.coordinates).map((item) => {
-      const coords = result.coordinates[item]
-      const parsedPath = path.parse(item)
-      const className = parsedPath.name
-
-      return (`.${className} {
-        background: url("./token-icons.webp") no-repeat;
-        width: ${coords.width}px;
-        height: ${coords.height}px;
-        display: inline-block;
-        background-position: ${coords.x}px ${coords.y}px;}`)
-    })
-
-    fs.writeFileSync(`${__dirname}/output/token-icons.css`, styles.join('\n\n'));
+      return {
+        ...curr,
+        [parsedPath.name]: coords
+      }
+    }, {})
 
     console.log(`[9/10] Making JSON descriptor file`)
 
     fs.writeFileSync(`${__dirname}/output/token-icons.json`, JSON.stringify({
       createdAt: Date.now(),
       webpFilename: 'token-icons.webp',
-      cssFilename: 'token-icons.css',
-      icons: images.map((item) => ({
-        filename: item,
-        token: item.split('-')[0],
-      }))
+      icons: images.map((item) => {
+        const parsedPath = path.parse(item)
+
+        const [token, address] = parsedPath.name.split('-')
+
+        return {
+          token,
+          address,
+          styles: {
+            width: `${styles[parsedPath.name].width}px`,
+            height: `${styles[parsedPath.name].height}px`,
+            backgroundPosition: `-${styles[parsedPath.name].x}px -${styles[parsedPath.name].y}px`,
+          }
+        }
+      })
     }, null, 2));
 
     console.log(`[10/10] Cleanup`)
 
     try {
-      // fs.rmSync(`${__dirname}/icons`, { recursive: true, force: true })
+      fs.rmSync(`${__dirname}/icons`, { recursive: true, force: true })
     } catch (e) {
       console.log('Error: ', e)
     }
