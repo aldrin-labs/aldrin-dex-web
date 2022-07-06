4d307d45b08ab437116b313f99f615e6fa7a330b
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-07-06 13:05:40 +0300

    upd route

--- src/routes/poolsRoute.tsx
+++ src/routes/poolsRoute.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
-// import { LiquidityPoolsMock } from '@sb/compositions/Pools/components/MockedScreen/LiquidityPoolsMock'
-import { PoolsComponent } from '@sb/compositions/Pools'
+
+import { PoolsComponent } from '@sb/compositions/PoolsV2'
 
 export default function PoolsRoutes({ match }) {
   return (
