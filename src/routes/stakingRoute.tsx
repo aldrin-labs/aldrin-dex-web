4b41dace9b28d4a4da46a1dd100cf2ca66fa7e70
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-08-25 16:17:43 +0300

    bump

--- src/routes/stakingRoute.tsx
+++ src/routes/stakingRoute.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import { StakingPage } from '@sb/compositions/StakingV2'
+// import { MaintenancePage } from '@sb/compositions/StakingV2/components/MaintenancePage'
 
 export default function StakingRoute({ match }) {
   return (
