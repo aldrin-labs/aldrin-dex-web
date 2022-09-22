93dcd930e9ba195d24eb95b4266413638663d2ed
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-09-22 13:22:03 +0300

    upd routes

--- src/routes/rinStakingRoute.tsx
+++ src/routes/rinStakingRoute.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import MaintenancePage from './plutoniansStakingRoute'
+// import MaintenancePage from './plutoniansStakingRoute'
 
-// import { RinStaking } from '@sb/compositions/RinStaking/index'
+import { RinStaking } from '@sb/compositions/RinStaking/index'
 
 export default function RinStakingRoute({ match }) {
   return (
     <Switch>
-      <Route path={match.url} component={MaintenancePage} />
+      <Route path={match.url} component={RinStaking} />
     </Switch>
   )
 }
--- src/routes/stakingRoute.tsx
+++ src/routes/stakingRoute.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import { MaintenancePage } from '@sb/compositions/StakingV2/components/MaintenancePage'
+// import { MaintenancePage } from '@sb/compositions/StakingV2/components/MaintenancePage'
 
-// import { StakingPage } from '@sb/compositions/Staking'
+import { StakingPage } from '@sb/compositions/Staking'
 
 export default function StakingRoute({ match }) {
   return (
     <Switch>
-      <Route path={match.url} component={MaintenancePage} />
+      <Route path={match.url} component={StakingPage} />
     </Switch>
   )
 }
